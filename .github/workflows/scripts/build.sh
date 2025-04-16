97fcf74d85b5cd8ab09e8261aa786e70834d3654
Christian Kadner <ckadner@us.ibm.com>
2025-04-15 20:15:28 -0800

    [CI] Cleanup files and file names
    Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -9,6 +9,7 @@ PATH=${cuda_home}/bin:$PATH
 LD_LIBRARY_PATH=${cuda_home}/lib64:$LD_LIBRARY_PATH
 
 # Install requirements
+# TODO: use `uv` ore remove
 $python_executable -m pip install -r requirements-build.txt -r requirements-cuda.txt
 
 # Limit the number of parallel jobs to avoid OOM
