47b9f0e0b2b78697c26a590a0a2329ae26b2a8c4
Prashant Gupta <prashantgupta@us.ibm.com>
2025-05-13 10:04:20 -0700

    use stop string instead
    it seems like this is fixed, it wasn't working earlier with engine core's step function
    
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- tests/e2e/test_spyre_cb.py
+++ tests/e2e/test_spyre_cb.py
@@ -5,7 +5,6 @@ Run `python -m pytest tests/test_spyre_cb.py`.
 
 import pytest
 from spyre_util import generate_cb_spyre_vllm_output, get_spyre_model_list
-from transformers import AutoTokenizer
 from vllm import EngineArgs, SamplingParams
 from vllm.v1.engine.llm_engine import LLMEngine as V1LLMEngine
 
@@ -106,19 +105,10 @@ def test_cb_with_steps(model: str, backend: str, cb: int,
         prompt2 = "10 9 8 7"
         prompt3 = "8 7 6 5"
 
-        tokenizer = AutoTokenizer.from_pretrained(model)
-        stop_token = "1"
-        # There's an issue with JackFram/llama-160m tokenizer it returns 2
-        # tokens for "1", the first is quotes, the second is the actual number
-        # 1. So, we always get the last token id which also works for the case
-        # the tokenizer return a single token id.
-        stop_token_ids = tokenizer.encode(stop_token,
-                                          add_special_tokens=False)[-1:]
-
         sampling_params = SamplingParams(
             max_tokens=max_tokens,
             temperature=0,
-            stop_token_ids=stop_token_ids,
+            stop="1",
             ignore_eos=True,
         )
 
