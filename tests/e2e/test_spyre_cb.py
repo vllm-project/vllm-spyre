7795c083f6350ba241c1af1e2b94074a512add11
Sophie du Couédic <sop@zurich.ibm.com>
2025-05-13 14:37:46 +0000

    use monkeypatch directly instead of creating a context
    Signed-off-by: Sophie du Couédic <sop@zurich.ibm.com>

--- tests/e2e/test_spyre_cb.py
+++ tests/e2e/test_spyre_cb.py
@@ -94,127 +94,126 @@ def test_cb_with_steps(model: str, backend: str,
     continuous batches of requests and one sequence
     an exit the batch while a new ones gets prefilled
     and appended to the batch"""
-    with monkeypatch.context() as m:
 
-        max_tokens = 20
-        max_num_seqs = 2  # defines max batch size
+    # set env vars
+    monkeypatch.setenv("VLLM_SPYRE_USE_CB", "1")
+    monkeypatch.setenv("VLLM_USE_V1", "1")
+    monkeypatch.setenv("VLLM_SPYRE_DYNAMO_BACKEND", backend)
 
-        prompt1 = "7 6 5 4"
-        prompt2 = "10 9 8 7"
-        prompt3 = "8 7 6 5"
+    # To get deterministic execution in V1
+    # and to enable InprocClient
+    monkeypatch.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", "0")
 
-        sampling_params = SamplingParams(
-            max_tokens=max_tokens,
-            temperature=0,
-            stop="1",
-            ignore_eos=True,
-        )
+    max_tokens = 20
+    max_num_seqs = 2  # defines max batch size
 
-        # set env vars
-        m.setenv("VLLM_SPYRE_USE_CB", "1")
-        m.setenv("VLLM_USE_V1", "1")
-        m.setenv("VLLM_SPYRE_DYNAMO_BACKEND", backend)
+    prompt1 = "7 6 5 4"
+    prompt2 = "10 9 8 7"
+    prompt3 = "8 7 6 5"
 
-        # To get deterministic execution in V1
-        # and to enable InprocClient
-        m.setenv("VLLM_ENABLE_V1_MULTIPROCESSING", "0")
+    sampling_params = SamplingParams(
+        max_tokens=max_tokens,
+        temperature=0,
+        stop="1",
+        ignore_eos=True,
+    )
 
-        # start the engine
-        engine_args = EngineArgs(model=model,
-                                 max_model_len=2048,
-                                 max_num_seqs=max_num_seqs)
+    # start the engine
+    engine_args = EngineArgs(model=model,
+                             max_model_len=2048,
+                             max_num_seqs=max_num_seqs)
 
-        engine = V1LLMEngine.from_engine_args(engine_args)
-        engine_core = engine.engine_core.engine_core
+    engine = V1LLMEngine.from_engine_args(engine_args)
+    engine_core = engine.engine_core.engine_core
 
-        # add first request
-        engine.add_request("1", prompt1, sampling_params)
+    # add first request
+    engine.add_request("1", prompt1, sampling_params)
 
-        assert len(engine_core.scheduler.waiting) == 1
-        assert len(engine_core.scheduler.running) == 0
+    assert len(engine_core.scheduler.waiting) == 1
+    assert len(engine_core.scheduler.running) == 0
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 1  # only 1 request
-        assert request_outputs[0].request_id == "1"  # req 1 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 1  # only 1 request
+    assert request_outputs[0].request_id == "1"  # req 1 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 1
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 1
 
-        # add another request
-        engine.add_request("2", prompt2, sampling_params)
+    # add another request
+    engine.add_request("2", prompt2, sampling_params)
 
-        assert len(engine_core.scheduler.waiting) == 1  # req 2 is waiting
-        assert len(engine_core.scheduler.running) == 1
+    assert len(engine_core.scheduler.waiting) == 1  # req 2 is waiting
+    assert len(engine_core.scheduler.running) == 1
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 1  # still only 1 request
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 1  # still only 1 request
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
-        assert request_outputs[1].request_id == "1"  # req 1 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    assert request_outputs[1].request_id == "1"  # req 1 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
-        assert request_outputs[1].request_id == "1"  # req 1 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    assert request_outputs[1].request_id == "1"  # req 1 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
-        assert request_outputs[1].request_id == "1"  # req 1 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    assert request_outputs[1].request_id == "1"  # req 1 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
 
-        # add the third request
-        # but since VLLM_SPYRE_MAX_BATCH_SIZE=2
-        # this request is waiting
-        engine.add_request("3", prompt3, sampling_params)
+    # add the third request
+    # but since VLLM_SPYRE_MAX_BATCH_SIZE=2
+    # this request is waiting
+    engine.add_request("3", prompt3, sampling_params)
 
-        assert len(engine_core.scheduler.waiting) == 1
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 1
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2  # both requests decoding now
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
-        assert request_outputs[1].request_id == "1"  # req 1 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2  # both requests decoding now
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    assert request_outputs[1].request_id == "1"  # req 1 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 1
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 1
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2  # both requests decoding now
-        assert request_outputs[0].request_id == "2"  # req 2 is decoding
-        assert request_outputs[1].request_id == "1"  # req 1 is decoding
-        assert request_outputs[1].finished  # request 1 is done
-        assert request_outputs[1].outputs[0].text == " 3 2 "
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2  # both requests decoding now
+    assert request_outputs[0].request_id == "2"  # req 2 is decoding
+    assert request_outputs[1].request_id == "1"  # req 1 is decoding
+    assert request_outputs[1].finished  # request 1 is done
+    assert request_outputs[1].outputs[0].text == " 3 2 "
 
-        assert len(engine_core.scheduler.waiting) == 1
-        assert len(engine_core.scheduler.running) == 1
+    assert len(engine_core.scheduler.waiting) == 1
+    assert len(engine_core.scheduler.running) == 1
 
-        # req 3 is scheduled now
-        request_outputs = engine.step()
-        assert len(request_outputs) == 1
-        assert request_outputs[0].request_id == "3"  # req 3 is decoding
+    # req 3 is scheduled now
+    request_outputs = engine.step()
+    assert len(request_outputs) == 1
+    assert request_outputs[0].request_id == "3"  # req 3 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
 
-        request_outputs = engine.step()
-        assert len(request_outputs) == 2  # requests 2 and 3 decoding now
-        assert request_outputs[0].request_id == "3"  # req 3 is decoding
-        assert request_outputs[1].request_id == "2"  # req 2 is decoding
+    request_outputs = engine.step()
+    assert len(request_outputs) == 2  # requests 2 and 3 decoding now
+    assert request_outputs[0].request_id == "3"  # req 3 is decoding
+    assert request_outputs[1].request_id == "2"  # req 2 is decoding
 
-        assert len(engine_core.scheduler.waiting) == 0
-        assert len(engine_core.scheduler.running) == 2
+    assert len(engine_core.scheduler.waiting) == 0
+    assert len(engine_core.scheduler.running) == 2
