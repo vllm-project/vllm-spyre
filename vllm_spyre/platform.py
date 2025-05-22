083b986c31daba1ec0f25f3f65fffbf127001ae1
Gabriel Marinho <gmarinho@ibm.com>
2025-05-22 14:19:38 -0300

    add maybe_update_max_tokens for SpyrePlatform
    Signed-off-by: Gabriel Marinho <gmarinho@ibm.com>

--- vllm_spyre/platform.py
+++ vllm_spyre/platform.py
@@ -276,3 +276,12 @@ class SpyrePlatform(Platform):
             if prompt_len <= shape['prompt_length']
             and max_tokens <= shape['new_tokens']
         ]
+
+    @classmethod
+    def maybe_update_default_max_tokens(self, prompt_len: int, default_max_tokens: int) -> int:
+        max_new_tokens = 1
+        for shape in self._warmup_shapes:
+            if prompt_len <= shape['prompt_length']:
+                max_new_tokens = max(max_new_tokens, shape['new_tokens'])
+        
+        return max_new_tokens
