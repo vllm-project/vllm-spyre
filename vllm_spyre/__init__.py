4e84c6841b8d519dc884044d5fce0bf6b16f2a49
Prashant Gupta <prashantgupta@us.ibm.com>
2025-06-09 12:43:18 -0700

    ✨ add top-level spyre version
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- vllm_spyre/__init__.py
+++ vllm_spyre/__init__.py
@@ -28,3 +28,13 @@ def _init_logging():
 
 
 _init_logging()
+
+try:
+    from ._version import __version__, __version_tuple__
+except Exception as e:
+    import warnings
+
+    warnings.warn(f"Failed to read commit hash:\n{e}", RuntimeWarning, stacklevel=2)
+
+    __version__ = "dev"
+    __version_tuple__ = (0, 0, __version__)
