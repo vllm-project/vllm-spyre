0674102a7d3c8de6d4e993a6475f3da6ecec5290
Sophie du Couédic <sop@zurich.ibm.com>
2025-06-11 21:17:16 +0000

    fix formatting
    Signed-off-by: Sophie du Couédic <sop@zurich.ibm.com>

--- examples/offline_inference/offline_inference_spyre_cb_test.py
+++ examples/offline_inference/offline_inference_spyre_cb_test.py
@@ -1,13 +1,13 @@
+import argparse
 import os
 import platform
 import time
-import argparse
-
 
 from vllm import LLM, SamplingParams
 
 # RUN with fms branch: https://github.com/foundation-model-stack/
 # foundation-model-stack/tree/paged_attn_mock
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,21 +16,22 @@ def main():
     parser.add_argument("--max_num_seqs", type=int, default=2)
     parser.add_argument("--tp", type=int, default=1)
     args = parser.parse_args()
-    
+
     max_tokens1 = 65
     max_tokens2 = 67
     max_tokens3 = 7
     max_num_seqs = args.max_num_seqs  # defines max batch size
 
     if platform.machine() == "arm64":
-        print("Detected arm64 running environment. "
+        print(
+            "Detected arm64 running environment. "
             "Setting HF_HUB_OFFLINE=1 otherwise vllm tries to download a "
             "different version of the model using HF API which might not work "
             "locally on arm64.")
         os.environ["HF_HUB_OFFLINE"] = "1"
 
     # defining here to be able to run/debug directly from VSC (not via terminal)
-    if not "VLLM_SPYRE_DYNAMO_BACKEND" in os.environ:
+    if "VLLM_SPYRE_DYNAMO_BACKEND" not in os.environ:
         os.environ['VLLM_SPYRE_DYNAMO_BACKEND'] = 'eager'
     os.environ['VLLM_SPYRE_USE_CB'] = '1'
     os.environ['VLLM_USE_V1'] = '1'
@@ -38,17 +39,19 @@ def main():
     # Sample prompts.
     template = (
         "Below is an instruction that describes a task. Write a response that "
-        "appropriately completes the request. Be polite in your response to the "
-        "user.\n\n### Instruction:\n{}\n\n### Response:")
+        "appropriately completes the request. Be polite in your response to "
+        "the user.\n\n### Instruction:\n{}\n\n### Response:")
 
     prompt1 = template.format(
-        "Provide a list of instructions for preparing chicken soup for a family "
-        "of four.")
+        "Provide a list of instructions for preparing chicken soup for a "
+        "family of four.")
 
-    prompt2 = template.format("Provide instructions for preparing chicken soup.")
+    prompt2 = template.format(
+        "Provide instructions for preparing chicken soup.")
 
     prompt3 = template.format(
-        "Provide a list of instructions for preparing chicken soup for a family.")
+        "Provide a list of instructions for preparing chicken soup for a family"
+        ".")
 
     prompts = [
         prompt1,
@@ -58,16 +61,16 @@ def main():
 
     # Create a sampling params object.
     sampling_params1 = SamplingParams(max_tokens=max_tokens1,
-                                    temperature=0.0,
-                                    ignore_eos=True)
+                                      temperature=0.0,
+                                      ignore_eos=True)
 
     sampling_params2 = SamplingParams(max_tokens=max_tokens2,
-                                    temperature=0.0,
-                                    ignore_eos=True)
+                                      temperature=0.0,
+                                      ignore_eos=True)
 
     sampling_params3 = SamplingParams(max_tokens=max_tokens3,
-                                    temperature=0.0,
-                                    ignore_eos=True)
+                                      temperature=0.0,
+                                      ignore_eos=True)
 
     sampling_params = [
         sampling_params1,
@@ -77,19 +80,19 @@ def main():
 
     # Create an LLM.
     llm = LLM(model=args.model,
-            tokenizer=args.model,
-            max_model_len=args.max_model_len,
-            block_size=2048,
-            max_num_seqs=max_num_seqs,
-            tensor_parallel_size=args.tp)
+              tokenizer=args.model,
+              max_model_len=args.max_model_len,
+              block_size=2048,
+              max_num_seqs=max_num_seqs,
+              tensor_parallel_size=args.tp)
 
-    # Generate texts from the prompts. The output is a list of RequestOutput objects
-    # that contain the prompt, generated text, and other information.
+    # Generate texts from the prompts. The output is a list of RequestOutput
+    # objects that contain the prompt, generated text, and other information.
     print("=============== GENERATE")
     t0 = time.time()
     outputs = llm.generate(prompts, sampling_params)
     print("Time elaspsed for %d tokens is %.2f sec" %
-        (len(outputs[0].outputs[0].token_ids), time.time() - t0))
+          (len(outputs[0].outputs[0].token_ids), time.time() - t0))
     print("===============")
     for output in outputs:
         print(output.outputs[0])
