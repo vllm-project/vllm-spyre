6158aae739b8a14d802eff6d99dfe6c23fe346eb
Prashant Gupta <prashantgupta@us.ibm.com>
2025-07-11 10:18:35 -0700

    🔥 remove extra id
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- tests/e2e/test_spyre_prompt_logprobs.py
+++ tests/e2e/test_spyre_prompt_logprobs.py
@@ -19,9 +19,9 @@ from vllm_spyre.platform import SpyrePlatform
 @pytest.mark.parametrize("backend", get_spyre_backend_list())
 @pytest.mark.parametrize("model", get_spyre_model_list())
 @pytest.mark.parametrize("tp_size", [
-    pytest.param(1, id="tp_size"),
-    pytest.param(2, marks=pytest.mark.multi, id="tp_size"),
-    pytest.param(4, marks=pytest.mark.multi, id="tp_size")
+    pytest.param(1),
+    pytest.param(2, marks=pytest.mark.multi),
+    pytest.param(4, marks=pytest.mark.multi)
 ],
                          ids=lambda val: f"TP({val})")
 def test_prompt_logprobs(
