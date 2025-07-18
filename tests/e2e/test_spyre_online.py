5a738e13e30b09fa3fc778d782b6c18d157d5796
Joe Runde <joe@joerun.de>
2025-07-18 08:59:59 -0600

    :bug: fix cb online test
    Signed-off-by: Joe Runde <joe@joerun.de>

--- tests/e2e/test_spyre_online.py
+++ tests/e2e/test_spyre_online.py
@@ -87,9 +87,11 @@ def test_openai_serving_gptq(remote_openai_server, model, backend,
                          [pytest.param(1, marks=pytest.mark.cb, id="cb")])
 @pytest.mark.parametrize("max_num_seqs", [2],
                          ids=lambda val: f"max_num_seqs({val})")
+@pytest.mark.parametrize("max_model_len", [256],
+                         ids=lambda val: f"max_model_len({val})")
 @pytest.mark.parametrize("backend", get_spyre_backend_list())
 def test_openai_serving_cb(remote_openai_server, model, backend, cb,
-                           max_num_seqs):
+                           max_num_seqs, max_model_len):
     """Test online serving with CB using the `vllm serve` CLI"""
 
     client = remote_openai_server.get_client()
