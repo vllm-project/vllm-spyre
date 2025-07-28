c452acce40a0b2a6b67cd0ba1de4427efb0fc70d
Max de Bayser <mbayser@br.ibm.com>
2025-07-28 16:12:21 -0300

    disable hack for newer version
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

--- vllm_spyre/platform.py
+++ vllm_spyre/platform.py
@@ -9,7 +9,7 @@ import sys
 if sys.platform.startswith("darwin"):
     if sys.modules.get('triton'):
         del sys.modules['triton']
-
+import inspect
 import math
 import operator
 import os
@@ -72,7 +72,11 @@ class SpyrePlatform(Platform):
         # TODO: temporary hack while BertModels
         # inherit SupportsV0Only in vllm upstream.
         from vllm.config import ModelConfig
-        ModelConfig.is_v1_compatible = is_v1_compatible
+
+        # no need to patch after the model_config change
+        if not 'model_config' in \
+                inspect.getfullargspec(ModelConfig.is_v1_compatible).args:
+            ModelConfig.is_v1_compatible = is_v1_compatible
         return cls._device_type
 
     @classmethod
