fb8011a5c625f90eec9f7f00d1a2957d5cd6ab1f
Yannick Schnider <Yannick.Schnider1@ibm.com>
2025-07-29 15:56:44 +0200

    [CB] Support pseudo batch size 1 for decode, adjust warmup (#287)
    ### [CB] enable warmup for batch size 1
    
    in #285 we wanted to allow batch size 1 for continuous batching.
    However, the warmup did not support batch size 1. With this small fix it
    does work on CPU at least. It does not currently compile on the card.
    Would need to compare graphs next...
    
    ---------
    
    Signed-off-by: Joe Runde <joe@joerun.de>
    Signed-off-by: Yannick Schnider <yannick.schnider1@ibm.com>
    Signed-off-by: Yannick Schnider <Yannick.Schnider1@ibm.com>
    Co-authored-by: Joe Runde <joe@joerun.de>

--- vllm_spyre/platform.py
+++ vllm_spyre/platform.py
@@ -204,8 +204,9 @@ class SpyrePlatform(Platform):
         # set env vars for torch_sendnn to consume
         os.environ["VLLM_DT_MAX_CONTEXT_LEN"] = str(
             vllm_config.model_config.max_model_len)
+        # min decode batch size is 2 due to symbolic shape constraint in torch
         os.environ["VLLM_DT_MAX_BATCH_SIZE"] = str(
-            vllm_config.scheduler_config.max_num_seqs)
+            max(vllm_config.scheduler_config.max_num_seqs, 2))
 
     @classmethod
     def use_all_gather(cls) -> bool:
