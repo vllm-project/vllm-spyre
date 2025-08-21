18b07d1fec35deadc343a16aa803b4a5311ac200
Max de Bayser <mbayser@br.ibm.com>
2025-08-21 15:08:06 -0300

    add tests
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

---
+++ tests/e2e/test_spyre_scoring.py
@@ -0,0 +1,40 @@
+import pytest
+from sentence_transformers import CrossEncoder
+from spyre_util import get_spyre_backend_list, patch_warmup_shapes
+from vllm import LLM
+
+
+@pytest.mark.parametrize("model", ["BAAI/bge-reranker-v2-m3"])
+@pytest.mark.parametrize(
+    "warmup_shape",
+    [  # (prompt_length/batch_size)
+        pytest.param((64, 4), marks=pytest.mark.basic),
+    ])
+@pytest.mark.parametrize("backend", get_spyre_backend_list())
+def test_output(
+    model: str,
+    warmup_shape: tuple[int, int],
+    backend: str,
+    monkeypatch,
+) -> None:
+
+    monkeypatch.setenv("VLLM_SPYRE_DYNAMO_BACKEND", backend)
+    patch_warmup_shapes([warmup_shape], monkeypatch)
+
+    query = "What is the capital of France?"
+    docs = [
+        "The capital of France is Paris.", "The capital of Germany is Berlin."
+    ]
+
+    ce_model = CrossEncoder(model)
+    ce_scores = ce_model.predict([(query, docs[0]), (query, docs[1])])
+
+    vllm_model = LLM(model=model)
+    vllm_outputs = vllm_model.score(query, docs)
+    vllm_scores = [o.outputs.score for o in vllm_outputs]
+
+    print(f"{ce_scores=}")
+    print(f"{vllm_scores=}")
+
+    assert ce_scores[0] == pytest.approx(vllm_scores[0], rel=0.01)
+    assert ce_scores[1] == pytest.approx(vllm_scores[1], rel=0.01)
