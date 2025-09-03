ab0b708ca018c69c85515bc4e82ae272a665dd25
Prashant Gupta <prashantgupta@us.ibm.com>
2025-09-03 12:23:50 -0700

    ♻️ move more stuff around
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- tests/llm_cache.py
+++ tests/llm_cache.py
@@ -1,15 +1,346 @@
 """Contains utilities for caching models (instantiated as vLLM endpoints) 
 across test cases, to speed up test runtime."""
 
-from typing import Optional
+import os
+import subprocess
+import sys
+import time
+from concurrent.futures import Executor
+from typing import Callable, Generic, Optional, TypeVar
 
+import openai
 import pytest
-from vllm import LLM
+import requests
+from vllm import LLM, EngineArgs
+from vllm.entrypoints.openai.cli_args import make_arg_parser
+from vllm.utils import FlexibleArgumentParser, get_open_port
 from vllm.v1.engine.core import EngineCore
 
-from tests.llm_cache_utils import (EngineCache, LLMCache, RemoteOpenAIServer,
-                                   RemoteOpenAIServerCache)
-from tests.spyre_util import DecodeWarmupShapes
+from tests.llm_cache_utils import force_engine_shutdown
+from tests.spyre_util import DecodeWarmupShapes, patch_environment
+
+T = TypeVar("T")
+
+## class definitions ##########################################
+
+
+class ModelCache(Generic[T]):
+
+    def __init__(self, teardown_method: Callable[[T], None] | None = None):
+        self._model: T | None = None
+        self._runtime_config: dict | None = None
+        self._past_runtime_configs: list[dict] = []
+
+        self.hits = 0
+        self.misses = 0
+
+        if not teardown_method:
+            self._teardown = lambda x: x.shutdown()
+        else:
+            self._teardown = teardown_method
+
+    def maybe_get(self, runtime_config: dict) -> T | None:
+        if runtime_config == self._runtime_config:
+            self.hits += 1
+            return self._model
+
+        self.misses += 1
+
+        print(f"\n\tModel cache miss for type [{self._type()}]")
+        print(f"Requested config: {runtime_config}")
+        print(f"Currently cached config {self._runtime_config}\n")
+
+        return None
+
+    def set(self, runtime_config: dict, model: T) -> T:
+        assert runtime_config not in self._past_runtime_configs, (
+            f"Runtime config {runtime_config} was previously cached for type "
+            f"[{self._type()}], error in test ordering!")
+        self._runtime_config = runtime_config
+        self._past_runtime_configs.append(self._runtime_config)
+        self._model = model
+
+        return self._model
+
+    def clear(self):
+        if self._model:
+            self._teardown(self._model)
+            self._model = None
+            self._runtime_config = None
+
+    def _type(self) -> type | None:
+        if hasattr(self, "__orig_class__"):
+            return self.__orig_class__.__args__[0]
+        return None
+
+
+class LLMCache:
+    """Caches a vllm.LLM for use in subsequent tests.
+
+    Only a single LLM can be cached at a time, as AIUs don't support loading
+    multiple models at once."""
+
+    def __init__(self):
+        self._cache: ModelCache[LLM] = ModelCache[LLM](
+            teardown_method=lambda x: force_engine_shutdown(x))
+
+    def get_cached_llm(
+        self,
+        model: str,
+        max_model_len: int,
+        tensor_parallel_size: int,
+        backend: str,
+        monkeypatch: pytest.MonkeyPatch,
+        warmup_shapes: DecodeWarmupShapes | None = None,
+        max_num_seqs: Optional[int] = None,
+        use_cb: bool = False,
+    ) -> LLM:
+        """Creates an LLM with the provided runtime configuration.
+
+        If the last LLM created matches the config, then returns the cached LLM
+        instead to reduce LLM instantiation overhead.
+        """
+        runtime_config = {
+            "model": model,
+            "tensor_parallel_size": tensor_parallel_size,
+            "backend": backend,
+            "use_cb": use_cb,
+        }
+        if use_cb:
+            runtime_config.update({
+                "max_model_len": max_model_len,
+                "max_num_seqs": max_num_seqs
+            })
+        else:
+            runtime_config.update({"warmup_shapes": tuple(warmup_shapes)})
+
+        # Always patch the environment so that it's consistent with the LLM
+        patch_environment(use_cb, warmup_shapes, backend, monkeypatch)
+
+        maybe_llm = self._cache.maybe_get(runtime_config)
+        if maybe_llm:
+            return maybe_llm
+        self.clear()
+
+        return self._cache.set(
+            runtime_config,
+            LLM(
+                model=model,
+                tokenizer=model,
+                max_model_len=max_model_len,
+                max_num_seqs=max_num_seqs,
+                tensor_parallel_size=tensor_parallel_size,
+            ),
+        )
+
+    def clear(self) -> None:
+        self._cache.clear()
+
+
+class EngineCache:
+    """Cache for continuous batching engines"""
+
+    def __init__(self):
+        self._cache: ModelCache[EngineCore] = ModelCache[EngineCore]()
+
+    def get_engine(
+        self,
+        model: str,
+        max_model_len: int,
+        max_num_seqs: int,
+        available_blocks: int,
+        backend: str,
+        monkeypatch,
+    ) -> EngineCore:
+        runtime_config = {
+            "model": model,
+            "max_model_len": max_model_len,
+            "max_num_seqs": max_num_seqs,
+            "available_blocks": available_blocks,
+        }
+
+        # Always patch the environment so that it's consistent with the engine
+        patch_environment(use_cb=True,
+                          warmup_shapes=None,
+                          backend=backend,
+                          monkeypatch=monkeypatch)
+
+        maybe_engine = self._cache.maybe_get(runtime_config)
+        if maybe_engine:
+            return maybe_engine
+        self.clear()
+
+        # Setup the engine
+        engine_args = EngineArgs(
+            model=model,
+            tokenizer=model,
+            max_model_len=max_model_len,
+            max_num_seqs=max_num_seqs,
+            num_gpu_blocks_override=available_blocks,
+        )
+        vllm_config = engine_args.create_engine_config()
+        executor_class = Executor.get_class(vllm_config)
+        return self._cache.set(
+            runtime_config,
+            EngineCore(vllm_config=vllm_config,
+                       executor_class=executor_class,
+                       log_stats=False),
+        )
+
+    def clear(self) -> None:
+        self._cache.clear()
+
+
+class RemoteOpenAIServer:
+    """Subprocess wrapper that boots a vllm server with `vllm serve` for testing
+    against"""
+
+    DUMMY_API_KEY = "token-abc123"  # vLLM's OpenAI server does not need API key
+
+    def __init__(
+        self,
+        model: str,
+        vllm_serve_args: list[str],
+        *,
+        env_dict: Optional[dict[str, str]] = None,
+        seed: Optional[int] = 0,
+        auto_port: bool = True,
+        max_wait_seconds: Optional[float] = None,
+    ) -> None:
+        # NB: This implementation does not ensure that the model is downloaded
+        # before booting the server, it should be used with models already
+        # cached on disk
+
+        if auto_port:
+            if "-p" in vllm_serve_args or "--port" in vllm_serve_args:
+                raise ValueError("You have manually specified the port "
+                                 "when `auto_port=True`.")
+
+            # Don't mutate the input args
+            vllm_serve_args = vllm_serve_args + [
+                "--port", str(get_open_port())
+            ]
+        if seed is not None:
+            if "--seed" in vllm_serve_args:
+                raise ValueError("You have manually specified the seed "
+                                 f"when `seed={seed}`.")
+
+            vllm_serve_args = vllm_serve_args + ["--seed", str(seed)]
+
+        parser = FlexibleArgumentParser(
+            description="vLLM's remote OpenAI server.")
+        parser = make_arg_parser(parser)
+        args = parser.parse_args(["--model", model, *vllm_serve_args])
+        self.host = str(args.host or "localhost")
+        self.port = int(args.port)
+
+        env = os.environ.copy()
+        if env_dict is not None:
+            env.update(env_dict)
+        self.proc = subprocess.Popen(
+            ["vllm", "serve", model, *vllm_serve_args],
+            env=env,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+        max_wait_seconds = max_wait_seconds or 600
+        self._wait_for_server(url=self.url_for("health"),
+                              timeout=max_wait_seconds)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.shutdown()
+
+    def shutdown(self):
+        self.proc.terminate()
+        try:
+            self.proc.wait(8)
+        except subprocess.TimeoutExpired:
+            # force kill if needed
+            self.proc.kill()
+
+    def _wait_for_server(self, *, url: str, timeout: float):
+        # run health check
+        start = time.time()
+        while True:
+            try:
+                if requests.get(url).status_code == 200:
+                    break
+            except Exception:
+                # this exception can only be raised by requests.get,
+                # which means the server is not ready yet.
+                # the stack trace is not useful, so we suppress it
+                # by using `raise from None`.
+                result = self.proc.poll()
+                if result is not None and result != 0:
+                    raise RuntimeError("Server exited unexpectedly.") from None
+
+                time.sleep(0.5)
+                if time.time() - start > timeout:
+                    raise RuntimeError(
+                        "Server failed to start in time.") from None
+
+    @property
+    def url_root(self) -> str:
+        return f"http://{self.host}:{self.port}"
+
+    def url_for(self, *parts: str) -> str:
+        return self.url_root + "/" + "/".join(parts)
+
+    def get_client(self, **kwargs):
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = 600
+        return openai.OpenAI(
+            base_url=self.url_for("v1"),
+            api_key=self.DUMMY_API_KEY,
+            max_retries=0,
+            **kwargs,
+        )
+
+    def get_async_client(self, **kwargs):
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = 600
+        return openai.AsyncOpenAI(
+            base_url=self.url_for("v1"),
+            api_key=self.DUMMY_API_KEY,
+            max_retries=0,
+            **kwargs,
+        )
+
+
+class RemoteOpenAIServerCache:
+
+    def __init__(self):
+        self._cache: ModelCache[RemoteOpenAIServer] = ModelCache[
+            RemoteOpenAIServer]()
+
+    def get_api_server(self, model: str, server_args: list[str],
+                       server_env: dict) -> RemoteOpenAIServer:
+        """Get or create a new OpenAI server for a given model. and config"""
+        runtime_config = {
+            "model": model,
+            "server_args": tuple(server_args),
+            "server_env": server_env,
+        }
+        maybe_server = self._cache.maybe_get(runtime_config)
+        if maybe_server:
+            return maybe_server
+        self.clear()
+
+        return self._cache.set(
+            runtime_config,
+            RemoteOpenAIServer(model=model,
+                               vllm_serve_args=server_args,
+                               env_dict=server_env),
+        )
+
+    def clear(self) -> None:
+        self._cache.clear()
+
+
+## class definitions ##########################################
 
 API_SERVER_CACHE = RemoteOpenAIServerCache()
 LLM_CACHE = LLMCache()
--- tests/llm_cache_utils.py
+++ tests/llm_cache_utils.py
@@ -1,23 +1,8 @@
-import os
-import subprocess
-import sys
-import time
-from typing import Callable, Generic, NamedTuple, Optional, TypeVar
+from typing import NamedTuple
 
-import openai
-import requests
-from vllm import LLM, EngineArgs
-from vllm.entrypoints.openai.cli_args import make_arg_parser
-from vllm.utils import FlexibleArgumentParser, get_open_port
-from vllm.v1.engine.core import EngineCore
-from vllm.v1.executor.abstract import Executor
-
-from vllm.v1.engine.core import EngineCore
 from vllm import LLM
-from typing import NamedTuple, Optional
-import pytest
 
-from tests.spyre_util import DecodeWarmupShapes, patch_environment
+from tests.spyre_util import DecodeWarmupShapes
 
 
 def force_engine_shutdown(llm: LLM):
@@ -164,9 +149,9 @@ class SortKey(NamedTuple):
         params = item.callspec.params
         if key in params:
             shapes = params[key]
-            SortKey._assert_param(
-                isinstance(shapes, list), "Warmup shape must be a list of tuples", item
-            )
+            SortKey._assert_param(isinstance(shapes, list),
+                                  "Warmup shape must be a list of tuples",
+                                  item)
             SortKey._assert_param(
                 isinstance(shapes[0], tuple),
                 "Warmup shape must be a list of tuples",
@@ -184,9 +169,8 @@ class SortKey(NamedTuple):
         params = item.callspec.params
         for key in TP_KEYS:
             if key in params:
-                SortKey._assert_param(
-                    isinstance(params[key], int), "tp size must be an int", item
-                )
+                SortKey._assert_param(isinstance(params[key], int),
+                                      "tp size must be an int", item)
                 return params[key]
         # Assume no TP if not set
         return 1
@@ -197,9 +181,8 @@ class SortKey(NamedTuple):
         params = item.callspec.params
         for key in MODEL_KEYS:
             if key in params:
-                SortKey._assert_param(
-                    isinstance(params[key], str), "model must be a string", item
-                )
+                SortKey._assert_param(isinstance(params[key], str),
+                                      "model must be a string", item)
                 return params[key]
         # No assumption about default model, we likely don't need an llm if this
         # isn't set
@@ -212,9 +195,8 @@ class SortKey(NamedTuple):
             # if isinstance(backend, tuple) and len(backend) == 1:
             #     backend = backend[0]
 
-            SortKey._assert_param(
-                isinstance(backend, str), "backend must be a string.", item
-            )
+            SortKey._assert_param(isinstance(backend, str),
+                                  "backend must be a string.", item)
             return backend
         # If backend isn't given then this is likely a spyre-only test
         return "sendnn"
@@ -223,332 +205,13 @@ class SortKey(NamedTuple):
     def _get_num_blocks(item) -> int:
         if "available_blocks" in item.callspec.params:
             blocks = item.callspec.params["available_blocks"]
-            SortKey._assert_param(
-                isinstance(blocks, int), "available_blocks must be an int.", item
-            )
+            SortKey._assert_param(isinstance(blocks, int),
+                                  "available_blocks must be an int.", item)
             return blocks
         # Most tests don't use this param
         return 0
 
     @staticmethod
     def _assert_param(condition, message, item):
-        assert condition, (
-            message + f"\n\n\tTest: {item.listnames()}"
-            f"\n\n\tParams: {item.callspec.params}"
-        )
-
-
-T = TypeVar("T")
-
-
-class ModelCache(Generic[T]):
-
-    def __init__(self, teardown_method: Callable[[T], None] | None = None):
-        self._model: T | None = None
-        self._runtime_config: dict | None = None
-        self._past_runtime_configs: list[dict] = []
-
-        self.hits = 0
-        self.misses = 0
-
-        if not teardown_method:
-            self._teardown = lambda x: x.shutdown()
-        else:
-            self._teardown = teardown_method
-
-    def maybe_get(self, runtime_config: dict) -> T | None:
-        if runtime_config == self._runtime_config:
-            self.hits += 1
-            return self._model
-
-        self.misses += 1
-
-        print(f"\n\tModel cache miss for type [{self._type()}]")
-        print(f"Requested config: {runtime_config}")
-        print(f"Currently cached config {self._runtime_config}\n")
-
-        return None
-
-    def set(self, runtime_config: dict, model: T) -> T:
-        assert runtime_config not in self._past_runtime_configs, (
-            f"Runtime config {runtime_config} was previously cached for type "
-            f"[{self._type()}], error in test ordering!"
-        )
-        self._runtime_config = runtime_config
-        self._past_runtime_configs.append(self._runtime_config)
-        self._model = model
-
-        return self._model
-
-    def clear(self):
-        if self._model:
-            self._teardown(self._model)
-            self._model = None
-            self._runtime_config = None
-
-    def _type(self) -> type | None:
-        if hasattr(self, "__orig_class__"):
-            return self.__orig_class__.__args__[0]
-        return None
-
-
-class LLMCache:
-    """Caches a vllm.LLM for use in subsequent tests.
-
-    Only a single LLM can be cached at a time, as AIUs don't support loading
-    multiple models at once."""
-
-    def __init__(self):
-        self._cache: ModelCache[LLM] = ModelCache[LLM](
-            teardown_method=lambda x: force_engine_shutdown(x)
-        )
-
-    def get_cached_llm(
-        self,
-        model: str,
-        max_model_len: int,
-        tensor_parallel_size: int,
-        backend: str,
-        monkeypatch: pytest.MonkeyPatch,
-        warmup_shapes: DecodeWarmupShapes | None = None,
-        max_num_seqs: Optional[int] = None,
-        use_cb: bool = False,
-    ) -> LLM:
-        """Creates an LLM with the provided runtime configuration.
-
-        If the last LLM created matches the config, then returns the cached LLM
-        instead to reduce LLM instantiation overhead.
-        """
-        runtime_config = {
-            "model": model,
-            "tensor_parallel_size": tensor_parallel_size,
-            "backend": backend,
-            "use_cb": use_cb,
-        }
-        if use_cb:
-            runtime_config.update(
-                {"max_model_len": max_model_len, "max_num_seqs": max_num_seqs}
-            )
-        else:
-            runtime_config.update({"warmup_shapes": tuple(warmup_shapes)})
-
-        # Always patch the environment so that it's consistent with the LLM
-        patch_environment(use_cb, warmup_shapes, backend, monkeypatch)
-
-        maybe_llm = self._cache.maybe_get(runtime_config)
-        if maybe_llm:
-            return maybe_llm
-        self.clear()
-
-        return self._cache.set(
-            runtime_config,
-            LLM(
-                model=model,
-                tokenizer=model,
-                max_model_len=max_model_len,
-                max_num_seqs=max_num_seqs,
-                tensor_parallel_size=tensor_parallel_size,
-            ),
-        )
-
-    def clear(self) -> None:
-        self._cache.clear()
-
-
-class RemoteOpenAIServer:
-    """Subprocess wrapper that boots a vllm server with `vllm serve` for testing
-    against"""
-
-    DUMMY_API_KEY = "token-abc123"  # vLLM's OpenAI server does not need API key
-
-    def __init__(
-        self,
-        model: str,
-        vllm_serve_args: list[str],
-        *,
-        env_dict: Optional[dict[str, str]] = None,
-        seed: Optional[int] = 0,
-        auto_port: bool = True,
-        max_wait_seconds: Optional[float] = None,
-    ) -> None:
-        # NB: This implementation does not ensure that the model is downloaded
-        # before booting the server, it should be used with models already
-        # cached on disk
-
-        if auto_port:
-            if "-p" in vllm_serve_args or "--port" in vllm_serve_args:
-                raise ValueError(
-                    "You have manually specified the port " "when `auto_port=True`."
-                )
-
-            # Don't mutate the input args
-            vllm_serve_args = vllm_serve_args + ["--port", str(get_open_port())]
-        if seed is not None:
-            if "--seed" in vllm_serve_args:
-                raise ValueError(
-                    "You have manually specified the seed " f"when `seed={seed}`."
-                )
-
-            vllm_serve_args = vllm_serve_args + ["--seed", str(seed)]
-
-        parser = FlexibleArgumentParser(description="vLLM's remote OpenAI server.")
-        parser = make_arg_parser(parser)
-        args = parser.parse_args(["--model", model, *vllm_serve_args])
-        self.host = str(args.host or "localhost")
-        self.port = int(args.port)
-
-        env = os.environ.copy()
-        if env_dict is not None:
-            env.update(env_dict)
-        self.proc = subprocess.Popen(
-            ["vllm", "serve", model, *vllm_serve_args],
-            env=env,
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-        )
-        max_wait_seconds = max_wait_seconds or 600
-        self._wait_for_server(url=self.url_for("health"), timeout=max_wait_seconds)
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.shutdown()
-
-    def shutdown(self):
-        self.proc.terminate()
-        try:
-            self.proc.wait(8)
-        except subprocess.TimeoutExpired:
-            # force kill if needed
-            self.proc.kill()
-
-    def _wait_for_server(self, *, url: str, timeout: float):
-        # run health check
-        start = time.time()
-        while True:
-            try:
-                if requests.get(url).status_code == 200:
-                    break
-            except Exception:
-                # this exception can only be raised by requests.get,
-                # which means the server is not ready yet.
-                # the stack trace is not useful, so we suppress it
-                # by using `raise from None`.
-                result = self.proc.poll()
-                if result is not None and result != 0:
-                    raise RuntimeError("Server exited unexpectedly.") from None
-
-                time.sleep(0.5)
-                if time.time() - start > timeout:
-                    raise RuntimeError("Server failed to start in time.") from None
-
-    @property
-    def url_root(self) -> str:
-        return f"http://{self.host}:{self.port}"
-
-    def url_for(self, *parts: str) -> str:
-        return self.url_root + "/" + "/".join(parts)
-
-    def get_client(self, **kwargs):
-        if "timeout" not in kwargs:
-            kwargs["timeout"] = 600
-        return openai.OpenAI(
-            base_url=self.url_for("v1"),
-            api_key=self.DUMMY_API_KEY,
-            max_retries=0,
-            **kwargs,
-        )
-
-    def get_async_client(self, **kwargs):
-        if "timeout" not in kwargs:
-            kwargs["timeout"] = 600
-        return openai.AsyncOpenAI(
-            base_url=self.url_for("v1"),
-            api_key=self.DUMMY_API_KEY,
-            max_retries=0,
-            **kwargs,
-        )
-
-
-class RemoteOpenAIServerCache:
-
-    def __init__(self):
-        self._cache: ModelCache[RemoteOpenAIServer] = ModelCache[RemoteOpenAIServer]()
-
-    def get_api_server(
-        self, model: str, server_args: list[str], server_env: dict
-    ) -> RemoteOpenAIServer:
-        """Get or create a new OpenAI server for a given model. and config"""
-        runtime_config = {
-            "model": model,
-            "server_args": tuple(server_args),
-            "server_env": server_env,
-        }
-        maybe_server = self._cache.maybe_get(runtime_config)
-        if maybe_server:
-            return maybe_server
-        self.clear()
-
-        return self._cache.set(
-            runtime_config,
-            RemoteOpenAIServer(
-                model=model, vllm_serve_args=server_args, env_dict=server_env
-            ),
-        )
-
-    def clear(self) -> None:
-        self._cache.clear()
-
-
-class EngineCache:
-    """Cache for continuous batching engines"""
-
-    def __init__(self):
-        self._cache: ModelCache[EngineCore] = ModelCache[EngineCore]()
-
-    def get_engine(
-        self,
-        model: str,
-        max_model_len: int,
-        max_num_seqs: int,
-        available_blocks: int,
-        backend: str,
-        monkeypatch,
-    ) -> EngineCore:
-        runtime_config = {
-            "model": model,
-            "max_model_len": max_model_len,
-            "max_num_seqs": max_num_seqs,
-            "available_blocks": available_blocks,
-        }
-
-        # Always patch the environment so that it's consistent with the engine
-        patch_environment(
-            use_cb=True, warmup_shapes=None, backend=backend, monkeypatch=monkeypatch
-        )
-
-        maybe_engine = self._cache.maybe_get(runtime_config)
-        if maybe_engine:
-            return maybe_engine
-        self.clear()
-
-        # Setup the engine
-        engine_args = EngineArgs(
-            model=model,
-            tokenizer=model,
-            max_model_len=max_model_len,
-            max_num_seqs=max_num_seqs,
-            num_gpu_blocks_override=available_blocks,
-        )
-        vllm_config = engine_args.create_engine_config()
-        executor_class = Executor.get_class(vllm_config)
-        return self._cache.set(
-            runtime_config,
-            EngineCore(
-                vllm_config=vllm_config, executor_class=executor_class, log_stats=False
-            ),
-        )
-
-    def clear(self) -> None:
-        self._cache.clear()
+        assert condition, (message + f"\n\n\tTest: {item.listnames()}"
+                           f"\n\n\tParams: {item.callspec.params}")
