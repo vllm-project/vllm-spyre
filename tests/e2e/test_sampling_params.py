38530fff78e6f93e60c52e4dad669668c5f05cb2
Gabriel Marinho <gmarinho@ibm.com>
2025-09-11 15:05:31 -0300

    tests passing with full model
    Signed-off-by: Gabriel Marinho <gmarinho@ibm.com>

--- tests/e2e/test_sampling_params.py
+++ tests/e2e/test_sampling_params.py
@@ -1,7 +1,5 @@
 import pytest
-from vllm import SamplingParams
-
-pytestmark = pytest.mark.full_model
+from vllm import LLM, SamplingParams
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -9,121 +7,146 @@ def setenv(backend, monkeypatch):
     monkeypatch.setenv("VLLM_SPYRE_DYNAMO_BACKEND", backend)
 
 
-def test_spyre_batch1_temperature(model, setenv, monkeypatch, use_llm_cache):
-    prompt = "The capital of the United Kingdom is"
-    params1 = SamplingParams(temperature=0.0, seed=8780, max_tokens=5)
-    params2 = SamplingParams(temperature=0.5, seed=8780, max_tokens=5)
-    params3 = SamplingParams(temperature=1.0, seed=8780, max_tokens=5)
+@pytest.fixture(scope="module")
+def spyre_model():
+    return LLM("ibm-granite/granite-3.3-8b-instruct")
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
-    output3 = model.generate(prompt, params3)[0]
+
+def test_spyre_batch1_temperature(spyre_model, setenv, monkeypatch,
+                                  use_llm_cache):
+    prompt = "The capital of the United Kingdom is"
+    params1 = SamplingParams(temperature=0.0, seed=8780, max_tokens=20)
+    params2 = SamplingParams(temperature=0.5, seed=8780, max_tokens=20)
+    params3 = SamplingParams(temperature=1.0, seed=8780, max_tokens=20)
+
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
+    output3 = spyre_model.generate(prompt, params3)[0]
 
     assert output1.outputs[0].text != output2.outputs[0].text
     assert output2.outputs[0].text != output3.outputs[0].text
 
 
-def test_spyre_batch1_max_tokens(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_max_tokens(spyre_model, setenv, monkeypatch,
+                                 use_llm_cache):
     prompt = "Count to twenty"
     params1 = SamplingParams(temperature=0, seed=8780, max_tokens=15)
     params2 = SamplingParams(temperature=0, seed=8780)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert len(output1.outputs[0].token_ids) == 15
     assert len(output2.outputs[0].token_ids) > 15
 
 
-def test_spyre_batch1_stop_sequence(model, setenv, monkeypatch, use_llm_cache):
-    tokenizer = model.get_tokenizer()
+def test_spyre_batch1_stop_sequence(spyre_model, setenv, monkeypatch,
+                                    use_llm_cache):
+    tokenizer = spyre_model.get_tokenizer()
     stop_str = "train"
     stop_word_id = tokenizer.encode(stop_str)
     prompt = "The best way to travel from Paris to Berlim is by "
 
     params1 = SamplingParams(stop=[stop_str],
                              max_tokens=20,
+                             seed=8780,
                              logit_bias={stop_word_id[0]: 100})
-    params2 = SamplingParams(max_tokens=20, logit_bias={stop_word_id[0]: 100})
+    params2 = SamplingParams(max_tokens=20,
+                             seed=8780,
+                             logit_bias={stop_word_id[0]: 100})
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert stop_str not in output1.outputs[0].text
     assert output1.outputs[0].finish_reason == 'stop'
     assert output2.outputs[0].finish_reason != 'stop'
 
 
-def test_spyre_batch1_presence_penalty(model, setenv, monkeypatch,
+def test_spyre_batch1_presence_penalty(spyre_model, setenv, monkeypatch,
                                        use_llm_cache):
-    prompt = "Repeat over and over again: one one"
+    prompt = "REPEAT OVER AND OVER AGAIN THE MINIMUM \
+              TIMES POSSIBLE: one one one one one"
+
     word = " one"
+    tokenizer = spyre_model.get_tokenizer()
+    word_id = tokenizer.encode(" one")
+    param1 = SamplingParams(presence_penalty=2.0,
+                            seed=8780,
+                            logit_bias={word_id[0]: 1},
+                            max_tokens=20)
+    param2 = SamplingParams(presence_penalty=-2.0,
+                            seed=8780,
+                            logit_bias={word_id[0]: 1},
+                            max_tokens=20)
 
-    param1 = SamplingParams(presence_penalty=2.0, max_tokens=20)
-    param2 = SamplingParams(presence_penalty=0.0, max_tokens=20)
-
-    with_penalty = model.generate(prompt, param1)[0]
-    no_penalty = model.generate(prompt, param2)[0]
+    with_penalty = spyre_model.generate(prompt, param1)[0]
+    no_penalty = spyre_model.generate(prompt, param2)[0]
 
     assert with_penalty.outputs[0].text.lower().count(word) < \
                               no_penalty.outputs[0].text.lower().count(word)
 
 
-def test_spyre_batch1_frequency_penalty(model, setenv, monkeypatch,
+def test_spyre_batch1_frequency_penalty(spyre_model, setenv, monkeypatch,
                                         use_llm_cache):
-    prompt = "Repeat just that word and nothing else \
-                over and over again: one one"
 
-    word = " one"
+    prompt = 'repeat the word hi ten times:'
+    word = " hi"
 
-    param1 = SamplingParams(frequency_penalty=2.0, max_tokens=20)
-    param2 = SamplingParams(frequency_penalty=0.0, max_tokens=20)
+    param1 = SamplingParams(frequency_penalty=2.0, seed=8780, max_tokens=20)
+    param2 = SamplingParams(frequency_penalty=-2.0, seed=8780, max_tokens=20)
 
-    output = model.generate(prompt, param1)[0]
-    no_penalty = model.generate(prompt, param2)[0]
+    output = spyre_model.generate(prompt, param1)[0]
+    no_penalty = spyre_model.generate(prompt, param2)[0]
 
     word_count = no_penalty.outputs[0].text.lower().count(word)
 
     assert output.outputs[0].text.lower().count(word) < word_count
 
 
-def test_spyre_batch1_n_generations(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_n_generations(spyre_model, setenv, monkeypatch,
+                                    use_llm_cache):
     prompt = "The three most popular sports in the world are: "
-    params = SamplingParams(n=3, max_tokens=20)
 
-    output = model.generate(prompt, params)[0]
+    params = SamplingParams(n=3, seed=8780, max_tokens=20)
+
+    output = spyre_model.generate(prompt, params)[0]
 
     assert len(output.outputs) == 3
     assert output.outputs[0].text != output.outputs[1].text
     assert output.outputs[1].text != output.outputs[2].text
 
 
-def test_spyre_batch1_top_p(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_top_p(spyre_model, setenv, monkeypatch, use_llm_cache):
     prompt = "The first three letters of the alphabet are"
-    params1 = SamplingParams(top_p=0.01, temperature=2, max_tokens=10)
-    params2 = SamplingParams(temperature=2, max_tokens=10)
+    params1 = SamplingParams(top_p=0.01,
+                             seed=8780,
+                             temperature=1,
+                             max_tokens=10)
+    params2 = SamplingParams(temperature=1, seed=8780, max_tokens=10)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
-    assert "'A', 'B', and 'C'" in output1.outputs[0].text
+    assert "A, B, and C" in output1.outputs[0].text
     assert output1.outputs[0].text != output2.outputs[0].text
 
 
-def test_spyre_batch1_top_k(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_top_k(spyre_model, setenv, monkeypatch, use_llm_cache):
     prompt = "The opposite of hot is"
     params1 = SamplingParams(top_k=1, seed=42, max_tokens=5)
     params2 = SamplingParams(seed=42, max_tokens=5)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert "cold" in output1.outputs[0].text.lower()
     assert output1.outputs[0].text != output2.outputs[0].text
 
 
-def test_spyre_batch1_logit_bias(model, setenv, monkeypatch, use_llm_cache):
-    tokenizer = model.get_tokenizer()
+def test_spyre_batch1_logit_bias(spyre_model, setenv, monkeypatch,
+                                 use_llm_cache):
+    tokenizer = spyre_model.get_tokenizer()
     banned_word = "train"
     forced_word = "plane"
 
@@ -136,14 +159,15 @@ def test_spyre_batch1_logit_bias(model, setenv, monkeypatch, use_llm_cache):
     prompt = "The fastest way to travel between continents is by "
     params1 = SamplingParams(temperature=0,
                              max_tokens=5,
+                             seed=8780,
                              logit_bias={
                                  banned_word_id: -100,
                                  forced_word_id: 100,
                              })
-    params2 = SamplingParams(temperature=0, max_tokens=5)
+    params2 = SamplingParams(temperature=0, seed=8780, max_tokens=5)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert banned_word not in output1.outputs[0].text.lower()
     assert forced_word in output1.outputs[0].text.lower()
@@ -151,29 +175,42 @@ def test_spyre_batch1_logit_bias(model, setenv, monkeypatch, use_llm_cache):
     assert output1.outputs[0].text != output2.outputs[0].text
 
 
-def test_spyre_batch1_min_tokens(model, setenv, monkeypatch, use_llm_cache):
-    prompt = "Answer only yes or no and do not explain further:\
-                 can computers count?"
+def test_spyre_batch1_min_tokens(spyre_model, setenv, monkeypatch,
+                                 use_llm_cache):
+    prompt = "What is the capital of the USA?"
+    tokenizer = spyre_model.get_tokenizer()
+    eos_id = tokenizer.eos_token_id
 
-    params1 = SamplingParams(seed=42, min_tokens=19, max_tokens=20)
-    params2 = SamplingParams(seed=42, max_tokens=20)
+    params1 = SamplingParams(min_tokens=19,
+                             logit_bias={eos_id: 50},
+                             seed=8780,
+                             max_tokens=20)
+    params2 = SamplingParams(seed=8780, logit_bias={eos_id: 50}, max_tokens=20)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert len(output1.outputs[0].token_ids) >= 19
     assert len(output2.outputs[0].token_ids) < 19
 
 
-def test_spyre_batch1_ignore_eos(model, setenv, monkeypatch, use_llm_cache):
-    prompt = "Answer only yes or no and do not explain \
-                further: can computers count?"
+def test_spyre_batch1_ignore_eos(spyre_model, setenv, monkeypatch,
+                                 use_llm_cache):
+    tokenizer = spyre_model.get_tokenizer()
+    eos_id = tokenizer.eos_token_id
+    prompt = "This is the end of the story"
 
-    params1 = SamplingParams(ignore_eos=True, max_tokens=20)
-    params2 = SamplingParams(ignore_eos=False, max_tokens=20)
+    params1 = SamplingParams(ignore_eos=True,
+                             logit_bias={eos_id: 50},
+                             seed=8780,
+                             max_tokens=20)
+    params2 = SamplingParams(ignore_eos=False,
+                             logit_bias={eos_id: 50},
+                             seed=8780,
+                             max_tokens=20)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert len(output1.outputs[0].token_ids) == 20
     assert len(output2.outputs[0].token_ids) != len(
@@ -183,47 +220,57 @@ def test_spyre_batch1_ignore_eos(model, setenv, monkeypatch, use_llm_cache):
     assert output2.outputs[0].finish_reason != 'length'
 
 
-def test_spyre_batch1_min_p(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_min_p(spyre_model, setenv, monkeypatch, use_llm_cache):
     prompt = "The opposite of black is"
-    params1 = SamplingParams(min_p=0.5, temperature=2, max_tokens=5)
-    params2 = SamplingParams(temperature=2, max_tokens=5)
+    params1 = SamplingParams(min_p=0.5, seed=8780, temperature=2, max_tokens=5)
+    params2 = SamplingParams(temperature=2, seed=8780, max_tokens=5)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert "white" in output1.outputs[0].text.lower()
     assert output1.outputs[0].text != output2.outputs[0].text
 
 
-def test_spyre_batch1_bad_words(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_bad_words(spyre_model, setenv, monkeypatch,
+                                use_llm_cache):
     prompt = "The capital of France is"
     params1 = SamplingParams(max_tokens=5,
                              temperature=0,
-                             bad_words=["Paris", "France"])
-    params2 = SamplingParams(max_tokens=5, temperature=0)
+                             seed=8780,
+                             bad_words=[" Paris", " Parisi", " France"])
+    params2 = SamplingParams(max_tokens=5, seed=8780, temperature=0)
 
-    output1 = model.generate(prompt, params1)[0]
-    output2 = model.generate(prompt, params2)[0]
+    output1 = spyre_model.generate(prompt, params1)[0]
+    output2 = spyre_model.generate(prompt, params2)[0]
 
     assert "Paris" not in output1.outputs[0].text
     assert "France" not in output1.outputs[0].text
     assert output1.outputs[0].text != output2.outputs[0].text
 
 
-def test_spyre_batch1_detokenize(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_detokenize(spyre_model, setenv, monkeypatch,
+                                 use_llm_cache):
     prompt = "Hello, world!"
-    params = SamplingParams(max_tokens=5, temperature=0, detokenize=False)
-    output = model.generate(prompt, params)[0]
+    params = SamplingParams(max_tokens=5,
+                            seed=8780,
+                            temperature=0,
+                            detokenize=False)
+    output = spyre_model.generate(prompt, params)[0]
 
     assert output.outputs[0].text == ""
     assert len(output.outputs[0].token_ids) > 0
 
 
-def test_spyre_batch1_logprobs(model, setenv, monkeypatch, use_llm_cache):
+def test_spyre_batch1_logprobs(spyre_model, setenv, monkeypatch,
+                               use_llm_cache):
     num_logprobs = 5
     prompt = "The sky is"
-    params = SamplingParams(max_tokens=5, temperature=0, logprobs=num_logprobs)
-    output = model.generate(prompt, params)[0]
+    params = SamplingParams(max_tokens=5,
+                            seed=8780,
+                            temperature=0,
+                            logprobs=num_logprobs)
+    output = spyre_model.generate(prompt, params)[0]
 
     completion_output = output.outputs[0]
 
