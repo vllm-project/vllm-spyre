a140bf591e3b43825749fcf602ed54d2d862532d
Prashant Gupta <prashantgupta@us.ibm.com>
2025-09-17 14:33:28 -0700

    🔥 remove bert support back-comp
    Signed-off-by: Prashant Gupta <prashantgupta@us.ibm.com>

--- tests/utils/test_upstream_compatibility.py
+++ tests/utils/test_upstream_compatibility.py
@@ -12,27 +12,6 @@ VLLM_VERSION = os.getenv("TEST_VLLM_VERSION", "default")
 
 
 @pytest.mark.cpu
-def test_vllm_bert_support():
-    '''
-    Test if the vllm version under test already has Bert support for V1
-    '''
-
-    from vllm.model_executor.models.bert import BertEmbeddingModel
-
-    bert_supports_v0_only = getattr(BertEmbeddingModel, "supports_v0_only",
-                                    False)
-
-    if VLLM_VERSION == "vLLM:main":
-        assert not bert_supports_v0_only
-    elif VLLM_VERSION == "vLLM:lowest":
-        assert bert_supports_v0_only, (
-            "The lowest supported vLLM version already"
-            "supports Bert in V1. Remove the compatibility workarounds.")
-        # The compat code introduced in the PR below can now be removed:
-        # https://github.com/vllm-project/vllm-spyre/pull/277
-
-
-@pytest.mark.cpu
 def test_model_config_task(model: ModelInfo):
 
     from vllm.engine.arg_utils import EngineArgs
--- vllm_spyre/platform.py
+++ vllm_spyre/platform.py
@@ -1,4 +1,3 @@
-import inspect
 import sys
 
 # When running this plugin on a Mac, we assume it's for local development
@@ -43,31 +42,12 @@ THREADING_ENVS = [
 ]
 
 
-class classproperty:
-
-    def __init__(self, func):
-        self.func = func
-
-    def __get__(self, instance, owner):
-        return self.func(owner)
-
-
-@property  # type: ignore
-def is_v1_compatible(self) -> bool:
-    architectures = getattr(self.hf_config, "architectures", [])
-    patterns = ["Bert", "Roberta"]
-    if any(pat in arch for arch in architectures for pat in patterns):
-        return True
-    import vllm.model_executor.models as me_models
-    return me_models.ModelRegistry.is_v1_compatible(architectures)
-
-
 class SpyrePlatform(Platform):
     _enum = PlatformEnum.OOT
 
     # "spyre" device_name no longer worked due to https://github.com/vllm-project/vllm/pull/16464
     device_name: str = "cpu"
-    _device_type: str = "cpu"
+    device_type: str = "cpu"
     # compressed-tensors supported by
     # https://github.com/foundation-model-stack/fms-model-optimizer/blob/main/fms_mo/aiu_addons/__init__.py
     supported_quantization: list[str] = ["gptq", "compressed-tensors"]
@@ -79,19 +59,6 @@ class SpyrePlatform(Platform):
     # TODO: see if this needs to be set
     # See vllm batched_count_greater_than method
     # simple_compile_backend: str = "eager"
-
-    @classproperty
-    def device_type(cls):
-        # TODO: temporary hack while BertModels
-        # inherit SupportsV0Only in vllm upstream.
-        import vllm.model_executor.models as me_models
-        from vllm.config import ModelConfig
-
-        # no need to patch after the model_config change
-        if 'model_config' not in \
-                inspect.getfullargspec(me_models.ModelRegistry.is_v1_compatible).args:
-            ModelConfig.is_v1_compatible = is_v1_compatible
-        return cls._device_type
 
     @classmethod
     def get_device_name(cls, device_id: int = 0) -> str:
