cddcea94933019f17b309aa2196f33777caf2182
Joe Runde <joe@joerun.de>
2025-10-10 12:45:29 -0600

    :art: fmt
    Signed-off-by: Joe Runde <joe@joerun.de>

--- vllm_spyre/v1/metrics/__init__.py
+++ vllm_spyre/v1/metrics/__init__.py
@@ -2,5 +2,6 @@ from .stats_logger import (FileStatLogger, file_stat_logger_factory,
                            patch_async_llm_stat_loggers)
 
 __all__ = [
-    patch_async_llm_stat_loggers, file_stat_logger_factory, FileStatLogger
+    "patch_async_llm_stat_loggers", "file_stat_logger_factory",
+    "FileStatLogger"
 ]
