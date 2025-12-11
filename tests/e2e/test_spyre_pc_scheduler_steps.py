15662650bd293a59c5541d9794ef37181cf57af9
Yannick Schnider <yannick.schnider1@ibm.com>
2025-12-11 13:45:06 +0000

    add unit test
    Signed-off-by: Yannick Schnider <yannick.schnider1@ibm.com>

--- tests/e2e/test_spyre_pc_scheduler_steps.py
+++ tests/e2e/test_spyre_pc_scheduler_steps.py
@@ -155,6 +155,119 @@ def test_prefix_hit_within_batch(model: ModelInfo, backend: str,
 @pytest.mark.parametrize("max_model_len", [256])
 @pytest.mark.parametrize("max_num_batched_tokens", [128])
 @pytest.mark.parametrize("available_blocks", [None])
+def test_block_deduplication_within_batch(model: ModelInfo, backend: str,
+                                          monkeypatch: pytest.MonkeyPatch,
+                                          set_random_seed, max_num_seqs: int,
+                                          max_model_len: int,
+                                          max_num_batched_tokens: int,
+                                          available_blocks: int):
+    """ Scenario where two equal sequences are scheduled. As both sequences
+    fit in a single chunk they have to be recomputed. However, we can write
+    the KV cache into the same first block as the prompts are identical.
+    Therefore we end up with a duplicated block in the block table despite
+    not having a prefix hit for this example. 
+
+    Configuration:
+        * max_num_seqs: 2
+        * number of prompts: 2
+            * 0: len = 70,  max tokens = 2, step joining = 0
+            * 1: len = 70, max tokens = 2, step joining = 0
+    """
+    monkeypatch.setenv("VLLM_SPYRE_CP_INTERLEAVE_STEPS", "0")
+
+    seqs_max_tokens = [2, 2]
+    prompts_lengths = [70, 70]
+    steps_add_reqs = [0, 0]
+    seeds = [0, 0]  # twice the same sequence
+
+    checked_steps = [
+        {
+            "step": 0,
+            "tkv": 0,
+            "waiting": ["0", "1"],
+            "running": [],
+            "request_outputs": [],
+            "n_reserved_blocks": 0,
+            "n_used_blocks": 0
+        },
+        {  # prefill chunk 1 seq 0
+            "step": 1,
+            "tkv": 70,
+            "waiting": ["1"],
+            "running": ["0"],
+            "request_outputs": ["0"],
+            "n_reserved_blocks": 2,
+            "n_used_blocks": 2,
+            "n_prefix_hits": 0,
+            "n_cached_blocks": 0
+        },
+        {  # prefill chunk 1 seq 1
+            # cannot use prefix, as the last chunk has to always be recomputed
+            "step": 2,
+            "tkv": 70,
+            "waiting": [],
+            "running": ["1", "0"],
+            "request_outputs": ["1"],
+            "n_reserved_blocks": 4,
+            "n_used_blocks": 4,
+            "n_prefix_hits": 0,
+            "n_cached_blocks": 0,
+            # assert here that the block table has only 3 distinct block ids,
+            # e.g. the first block is shared among the two sequences 0 and 1
+            "n_shared_blocks": 1  # TODO
+        },
+        {
+            # Decode 1 of request 0.
+            # Decode 1 of request 1.
+            "step": 3,
+            "tkv": 71,
+            "waiting": [],
+            "running": [],
+            "request_outputs": ["1", "0"],
+            "finished_requests": ["1", "0"],
+            "n_reserved_blocks": 4,
+            "n_used_blocks": 4
+        },
+        {
+            # Tkv should be cleared one step later
+            "step": 4,
+            "tkv": 0,
+            "waiting": [],
+            "running": [],
+            "request_outputs": [],
+            "n_reserved_blocks": 0,
+            "n_used_blocks": 0
+        },
+    ]
+
+    check_scheduler_inference_steps(
+        model=model,
+        backend=backend,
+        monkeypatch=monkeypatch,
+        seqs_max_tokens=seqs_max_tokens,
+        prompts_lengths=prompts_lengths,
+        steps_add_reqs=steps_add_reqs,
+        checked_steps=checked_steps,
+        max_num_seqs=max_num_seqs,
+        max_model_len=max_model_len,
+        available_blocks=available_blocks,
+        use_cb=False,
+        random_prompts=True,
+        max_num_batched_tokens=max_num_batched_tokens,
+        prefix_caching=True,
+        seeds=seeds,
+    )
+
+
+@pytest.mark.cpu
+@pytest.mark.chunked_prefill
+@pytest.mark.full_model
+@pytest.mark.prefix_caching
+# These values are all parameterized for test sorting
+@pytest.mark.parametrize("max_num_seqs", [2])
+@pytest.mark.parametrize("max_model_len", [256])
+@pytest.mark.parametrize("max_num_batched_tokens", [128])
+@pytest.mark.parametrize("available_blocks", [None])
 def test_prefix_hit_not_in_batch(model: ModelInfo, backend: str,
                                  monkeypatch: pytest.MonkeyPatch,
                                  set_random_seed, max_num_seqs: int,
