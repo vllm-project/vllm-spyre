b7ba5c0e512634ff8fc951fccb4ea80fc889ed49
Alex-Brooks <Alex.Brooks@ibm.com>
2025-12-18 19:40:10 +0000

    unwrap llava next kv cache settings
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm_spyre/multimodal/utils.py
+++ vllm_spyre/multimodal/utils.py
@@ -4,6 +4,8 @@ Super hacky utils for multimodal model stuff.
 from fms import models
 from fms.utils.config import ModelConfig
 
+MULITMODAL_ARCHITECTURES = ["llava_next"]
+
 def is_multimodal(fms_obj, fms_mm_registry):
     """Used to check in an object is multimodal; This could be a
     ModelConfig or a model object, depending on the wrapping partial
@@ -25,3 +27,23 @@ def resolve_multimodal_vocab_size(fms_config: ModelConfig):
     if isinstance(fms_config, models.llava_next.LlavaNextConfig):
         return fms_config.text_config.src_vocab_size
     raise ValueError("Unable to resolve vocab size for multimodal config of type %s", type(fms_config))
+
+def unwrap_mm_kv_cache_opts(fms_config, hf_config):
+    """Unwrap options to be passed for the kv cache from the underlying
+    text configs and return the resulting dictionary, which is used to
+    .update() the common kv cache opts that don't need unwrapping.
+    
+    Currently this only handles llava next with granite llms, because
+    the LLM is abstracted in HF Transformers, but hardcoded to granite in
+    FMS. TODO: refactor this and ContinuousBatchingFmsModel's init
+    logic to make it generic for LLMs so that we can abstract it here.
+    """
+    if hf_config.model_type != "llava_next" or hf_config.text_config.model_type != "granite":
+        raise TypeError("Currently multimodal only supports granite vision with granite llms")
+
+    kv_cache_specs = {}
+    kv_cache_specs['num_layers'] = hf_config.text_config.num_hidden_layers
+    kv_cache_specs['head_dim'] = getattr(
+        fms_config.text_config, "head_dim",
+        hf_config.text_config.hidden_size // hf_config.text_config.num_attention_heads)
+    return kv_cache_specs
