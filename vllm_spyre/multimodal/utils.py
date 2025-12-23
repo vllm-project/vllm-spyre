f334888a9b712bfa90e5de774ef37f9a9280e219
Alex-Brooks <Alex.Brooks@ibm.com>
2025-12-23 00:33:51 +0000

    Add utils for llava next warmup
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- vllm_spyre/multimodal/utils.py
+++ vllm_spyre/multimodal/utils.py
@@ -1,10 +1,19 @@
 """
 Super hacky utils for multimodal model stuff.
 """
+import torch
 from fms import models
 from fms.utils import serialization
 from fms.utils.config import ModelConfig
 from transformers import LlavaNextConfig
+
+from vllm.multimodal.inputs import (
+    MultiModalFeatureSpec,
+    MultiModalKwargsItem,
+    MultiModalBatchedField,
+    MultiModalFieldElem,
+    PlaceholderRange,
+)
 
 MULITMODAL_ARCHITECTURES = ["llava_next"]
 
@@ -68,3 +77,57 @@ def get_mm_specific_load_overrides(model_config):
             "text_config": {"head_dim": 128},
         }
     return get_model_kwargs
+
+def get_multimodal_warmup_features(valid_token_ids):
+    # Multimodal models take embeddings as inputs
+    # since we merge multimodal features with them;
+    # these take priority over input toks and skip
+    # embed in the FMS model.
+    mm_features = get_llava_next_image_features()
+    warmup_input_ids = get_llava_next_text_features()
+    warmup_embeds_tensor = torch.rand((3, warmup_input_ids.shape[-1], 4096))
+
+    # Input features for the visual encoder
+    return mm_features, warmup_input_ids, warmup_embeds_tensor
+
+def get_llava_next_image_features():
+    # This is the minimal (small) image case in granite vision;
+    # The maximal shape has 11 tiles. Careful to handle the offsets
+    # correctly if this is modified!
+    pixel_values_shape = [3, 3, 384, 384]
+    # Image size (px, not tokens)
+    image_sizes = torch.tensor([118, 177], dtype=torch.int64)
+    # offsets wrt text prompt
+    mm_position = PlaceholderRange(offset=42, length=1836)
+
+    mm_features = [
+        MultiModalFeatureSpec(
+            data=MultiModalKwargsItem({
+                "pixel_values": MultiModalFieldElem(
+                    modality="image",
+                    key="pixel_values",
+                    data=torch.rand(
+                        pixel_values_shape,
+                        dtype=torch.bfloat16,
+                    ),
+                    field=MultiModalBatchedField()
+                ),
+                "image_sizes": MultiModalFieldElem(
+                    modality="image",
+                    key="image_sizes",
+                    data=image_sizes,
+                    field=MultiModalBatchedField()
+                )
+            }),
+            modality="image",
+            identifier="MM-warmup-llava-next",
+            mm_position=mm_position,
+        )
+    ]
+    return mm_features
+
+def get_llava_next_text_features():
+    # TODO make this less hacky, build dynamically, don't hardcode image token
+    img_toks = [49155] * 1836
+    return torch.tensor([46, 110, 2946, 28318, 203, 51, 11210, 3733, 312, 39489, 1256, 461, 600, 5549, 31251, 629, 21488, 47330, 32, 886, 47330, 13344, 17247, 30, 16360, 30, 461, 7743, 659, 19969, 372, 322, 1256, 1182, 10017, 32, 203, 46, 110, 496, 28318, 203]
+                        + img_toks + [203, 7628, 458, 1778, 32, 203, 46, 110, 17594, 28318, 203])
