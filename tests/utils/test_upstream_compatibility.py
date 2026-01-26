3c7d319dd9d75bafb8aeb9f6f4f5c39b8e9453df
Travis Johnson <tsjohnso@us.ibm.com>
2026-01-26 13:59:53 -0700

    :fire: remove vllm 0.10.2 compatibility
    Signed-off-by: Travis Johnson <tsjohnso@us.ibm.com>

--- tests/utils/test_upstream_compatibility.py
+++ tests/utils/test_upstream_compatibility.py
@@ -2,38 +2,14 @@ import os
 
 import pytest
 from vllm.v1.core.block_pool import BlockPool
-from vllm.v1.core.sched.output import NewRequestData, SchedulerOutput
+from vllm.v1.core.sched.output import SchedulerOutput
 from vllm.v1.core.single_type_kv_cache_manager import FullAttentionManager
-from vllm.v1.kv_cache_interface import FullAttentionSpec
 
 from vllm_spyre.compat_utils import dataclass_fields, has_argument
 
 pytestmark = pytest.mark.compat
 
 VLLM_VERSION = os.getenv("TEST_VLLM_VERSION", "default")
-
-
-def test_mm_inputs():
-    if VLLM_VERSION == "vLLM:lowest":
-        # Can remove "mm_kwargs", "mm_hashes", "mm_positions"
-        # (replaced by mm_features)
-        assert "mm_kwargs" in dataclass_fields(NewRequestData)
-
-
-def test_get_sampler():
-    if VLLM_VERSION == "vLLM:lowest":
-        try:
-            from vllm.model_executor.layers.sampler import (  # # noqa #ty: ignore
-                get_sampler,
-            )
-        except ImportError as e:
-            raise AssertionError("Remove backwards compatibility for get_sampler") from e
-
-
-def test_use_mla():
-    if VLLM_VERSION == "vLLM:lowest":
-        # Can remove backwards compatibility for use_mla
-        assert "use_mla" in dataclass_fields(FullAttentionSpec)
 
 
 def test_pin_memory_available():
--- tests/v1/worker/mock_model.py
+++ tests/v1/worker/mock_model.py
@@ -30,13 +30,7 @@ class MockSpyreCausalLM:
         self,
         vllm_config: VllmConfig,
     ) -> None:
-        try:
-            ## Temporary backwards compatibility for 0.10.2
-            from vllm.model_executor.layers.sampler import get_sampler
-
-            self.sampler = get_sampler()
-        except (ImportError, ModuleNotFoundError):
-            self.sampler = Sampler()
+        self.sampler = Sampler()
 
         # boolean tensor of length batch size with indices:
         # True for unfinished sequences and
