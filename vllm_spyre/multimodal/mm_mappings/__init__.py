ba050c73e76d64daf593ba8acdcd80999fd624b9
Gaurav-Kumbhat <Gaurav.Kumbhat@ibm.com>
2026-02-05 07:48:13 -0800

    :construction_worker: Initiate mistral3 multimodal support
    Signed-off-by: Gaurav-Kumbhat <Gaurav.Kumbhat@ibm.com>

--- vllm_spyre/multimodal/mm_mappings/__init__.py
+++ vllm_spyre/multimodal/mm_mappings/__init__.py
@@ -1,4 +1,5 @@
 from vllm_spyre.multimodal.mm_mappings.base import MMUtilsBase, MMWarmupInputs
 from vllm_spyre.multimodal.mm_mappings.llava_next import LlavaNextMMUtils
+from vllm_spyre.multimodal.mm_mappings.mistral3 import Mistral3MMUtils
 
-__all__ = ["MMWarmupInputs", "MMUtilsBase", "LlavaNextMMUtils"]
+__all__ = ["MMWarmupInputs", "MMUtilsBase", "LlavaNextMMUtils", "Mistral3MMUtils"]
