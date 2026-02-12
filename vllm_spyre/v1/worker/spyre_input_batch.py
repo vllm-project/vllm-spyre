5293a6b34df4e14fdd176fdb71510d7a11bbc558
Max de Bayser <mbayser@br.ibm.com>
2026-02-12 19:05:18 -0300

    remove prompt_logprobs from input batch
    Signed-off-by: Max de Bayser <mbayser@br.ibm.com>

--- vllm_spyre/v1/worker/spyre_input_batch.py
+++ vllm_spyre/v1/worker/spyre_input_batch.py
@@ -323,9 +323,6 @@ class SamplingInputBatch(BaseInputBatch[SamplingRequestState]):
         self.generators: dict[int, torch.Generator] = {}
 
         self.num_logprobs: dict[str, int] = {}
-        # NOTE(rob): num_prompt_logprobs only includes reqs
-        # that are currently in the prefill phase.
-        self.num_prompt_logprobs: dict[str, int] = {}
 
         # Internal representation of per-step batch state changes, used for
         # reordering persistent batch and generating logitsprocs batch state
@@ -477,8 +474,6 @@ class SamplingInputBatch(BaseInputBatch[SamplingRequestState]):
 
         if sampling_params.logprobs is not None:
             self.num_logprobs[req_id] = sampling_params.logprobs
-        if sampling_params.prompt_logprobs is not None:
-            self.num_prompt_logprobs[req_id] = sampling_params.prompt_logprobs
 
         if sampling_params.allowed_token_ids:
             self.has_allowed_token_ids.add(req_id)
@@ -510,7 +505,6 @@ class SamplingInputBatch(BaseInputBatch[SamplingRequestState]):
         self.repetition_penalties_reqs = set()
         self.generators = {}
         self.num_logprobs = {}
-        self.num_prompt_logprobs = {}
 
         self.has_allowed_token_ids = set()
         if self.allowed_token_ids_mask is not None:
@@ -566,7 +560,6 @@ class SamplingInputBatch(BaseInputBatch[SamplingRequestState]):
         self.repetition_penalties_reqs.discard(req_id)
         self.generators.pop(req_index, None)
         self.num_logprobs.pop(req_id, None)
-        self.num_prompt_logprobs.pop(req_id, None)
 
         self.has_allowed_token_ids.discard(req_id)
 
@@ -693,10 +686,6 @@ class SamplingInputBatch(BaseInputBatch[SamplingRequestState]):
     @property
     def max_num_logprobs(self) -> int | None:
         return max(self.num_logprobs.values()) if self.num_logprobs else None
-
-    @property
-    def no_prompt_logprob(self) -> bool:
-        return not self.num_prompt_logprobs
 
     @property
     def no_allowed_token_ids(self) -> bool:
