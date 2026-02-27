bfbc64aba71c3aca3e8733d332d7eb14436546b7
Thomas Ortner <boh@zurich.ibm.com>
2026-02-27 16:15:07 +0000

    Changed filename
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- vllm_spyre_next/vllm_spyre_next/platform.py
+++ vllm_spyre_next/vllm_spyre_next/platform.py
@@ -39,7 +39,7 @@ class TorchSpyrePlatform(CpuPlatform):
     # Register the PyTorch Native Paged Attention implementation as the CUSTOM backend
     register_backend(
         AttentionBackendEnum.CUSTOM,
-        "vllm_spyre_next.v1.attention.backends.pytorch_native_paged_attn.SpyreAttentionPagedBackend",
+        "vllm_spyre_next.v1.attention.backends.spyre_paged_attn.SpyreAttentionPagedBackend",
     )
 
     @classmethod
