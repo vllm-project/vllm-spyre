c97e8db3ee7b97878daca05546f1c5b78f1d2955
Thomas Ortner <boh@zurich.ibm.com>
2026-02-27 11:37:01 +0000

    Integrated custom attention backend
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- vllm_spyre_next/vllm_spyre_next/platform.py
+++ vllm_spyre_next/vllm_spyre_next/platform.py
@@ -16,6 +16,7 @@ if sys.platform.startswith("darwin"):
 from vllm.logger import init_logger
 from vllm.platforms import PlatformEnum
 from vllm.platforms.cpu import CpuPlatform
+from vllm.v1.attention.backends.registry import AttentionBackendEnum, register_backend
 
 if TYPE_CHECKING:
     # NB: We can't eagerly import many things from vllm since vllm.config
@@ -33,6 +34,9 @@ class TorchSpyrePlatform(CpuPlatform):
     # "spyre" device_name no longer worked due to https://github.com/vllm-project/vllm/pull/16464
     device_name: str = "cpu"
     device_type: str = "cpu"
+    
+    # Register the PyTorch Native Paged Attention implementation as the CUSTOM backend
+    register_backend(AttentionBackendEnum.CUSTOM, "vllm_spyre_next.v1.attention.backends.pytorch_native_paged_attn.PyTorchNativePagedAttentionBackend")
 
     @classmethod
     def get_device_name(cls, device_id: int = 0) -> str:
@@ -71,6 +75,18 @@ class TorchSpyrePlatform(CpuPlatform):
         model_name = vllm_config.model_config.model
 
         logger.info(message, _version.version, model_name)
+
+    @classmethod
+    def get_attn_backend_cls(
+        cls,
+        selected_backend,
+        attn_selector_config,
+        num_heads = None,
+    ) -> str:
+        if selected_backend == AttentionBackendEnum.CUSTOM:
+            return AttentionBackendEnum.CUSTOM.get_path()
+        else:
+            return super().get_attn_backend_cls(selected_backend, attn_selector_config, num_heads)
 
     @classmethod
     def check_and_update_config(cls, vllm_config: VllmConfig) -> None:
