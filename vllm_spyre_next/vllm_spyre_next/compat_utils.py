139ab4afe49a0de581fa4c13a899e5f4cec94d67
Jan van Lunteren <jvl@zurich.ibm.com>
2026-03-24 06:43:10 -0400

    formatting
    Signed-off-by: Jan van Lunteren <jvl@zurich.ibm.com>

--- vllm_spyre_next/vllm_spyre_next/compat_utils.py
+++ vllm_spyre_next/vllm_spyre_next/compat_utils.py
@@ -1,9 +1,5 @@
-import inspect
 from dataclasses import fields
-from functools import lru_cache
-from typing import Callable
 
 
 def dataclass_fields(cls: type) -> list[str]:
     return [f.name for f in fields(cls)]
-
