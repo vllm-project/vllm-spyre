a8c26f615c6f366a4b8c0b051b75338384823214
Jan van Lunteren <jvl@zurich.ibm.com>
2026-03-25 03:53:41 -0400

    limit supported data types to float16
    Signed-off-by: Jan van Lunteren <jvl@zurich.ibm.com>

--- vllm_spyre_next/vllm_spyre_next/v1/attention/backends/spyre_attn.py
+++ vllm_spyre_next/vllm_spyre_next/v1/attention/backends/spyre_attn.py
@@ -116,8 +116,6 @@ class SpyreAttentionPagedBackend(AttentionBackend):
     accept_output_buffer: bool = True
     supported_dtypes: ClassVar[list[torch.dtype]] = [
         torch.float16,
-        torch.bfloat16,
-        torch.float32,
     ]
     supported_kv_cache_dtypes: ClassVar[list[CacheDType]] = [
         "auto",
