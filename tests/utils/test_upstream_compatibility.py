c8366c449f321b6d2d6f61fb34eb41464fd66ef3
Rafael Vasquez <rafvasq21@gmail.com>
2026-04-07 15:55:53 -0400

    Restore empty upstream compat test file
    Signed-off-by: Rafael Vasquez <rafvasq21@gmail.com>

---
+++ tests/utils/test_upstream_compatibility.py
@@ -0,0 +1,17 @@
+"""
+Tests checking for vLLM upstream compatibility requirements.
+
+As we remove support for old vLLM versions, we want to keep track of the
+compatibility code that can be cleaned up.
+"""
+
+import os
+
+import pytest
+
+pytestmark = pytest.mark.compat
+
+VLLM_VERSION = os.getenv("TEST_VLLM_VERSION", "default")
+
+# We reset the minimum version, so no tests currently
+# Check git history of this file for examples of the tests
