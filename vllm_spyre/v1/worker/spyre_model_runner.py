e0695b428ec77957b3aca0982b5022acbc1a4fd6
Rehan Khan <Rehan.Khan7@ibm.com>
2026-04-07 14:20:58 +0530

    [bugfix] Fix token degradation issue
    Signed-off-by: Rehan Khan <Rehan.Khan7@ibm.com>

--- vllm_spyre/v1/worker/spyre_model_runner.py
+++ vllm_spyre/v1/worker/spyre_model_runner.py
@@ -2382,7 +2382,7 @@ class ChunkedPrefillModelRunner(ContinuousBatchingSpyreModelRunner):
         assert sampling_params is not None, "sampling_params are required for this model runner"
         assert prompt_token_ids is not None, "prompt token ids are required for this model runner"
 
-        is_new_batch = self.get_n_free_blocks() == self.n_blocks
+        is_new_batch = self.input_batch._num_requests == 0
         prompt_len = len(prompt_token_ids)
         mm_features = getattr(request, "mm_features", None)
 
