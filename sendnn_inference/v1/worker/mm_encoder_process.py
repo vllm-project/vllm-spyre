b3dd1bb1638f7ac07c7983704bf0054fd0fefe2d
Kevin Tu <ktu@ibm.com>
2026-08-03 09:13:21 -0500

    Fix issue with math.ceil and potential None arg
    Signed-off-by: Kevin Tu <ktu@ibm.com>

--- sendnn_inference/v1/worker/mm_encoder_process.py
+++ sendnn_inference/v1/worker/mm_encoder_process.py
@@ -164,7 +164,10 @@ def _configure_encoder_threads() -> None:
         encoder_cpu_count = None
     elif platform.machine() == "ppc64le":
         cpu_count, _ = SpyrePlatform.get_cpu_count(use_logical_cpus=True)
-        encoder_cpu_count = min(math.ceil(cpu_count), 36)
+        if cpu_count is None:
+            encoder_cpu_count = None
+        else:
+            encoder_cpu_count = min(math.ceil(cpu_count), 36)
     else:
         encoder_cpu_count = math.ceil(cpu_count)
 
