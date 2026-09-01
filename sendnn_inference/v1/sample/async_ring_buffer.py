dmholtz/sendnn-inference

feat(sampling): Add async ring buffer and Spyre-optimized samplers
Implement asynchronous exponential noise pre-sampling to optimize the
latency-critical sampling path for Spyre hardware.

sendnn_inference/v1/sample/async_ring_buffer.py
@@ -1,0 +1,160 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the sendnn-inference project
+
+import contextlib
+import queue
+import threading
+from abc import ABC, abstractmethod
+from collections.abc import Generator
+
+import torch
+
+
+class AsyncRingBuffer(ABC):
+    """Pre-generates data rows on a background thread via a ring buffer.
+
+    Maintains a contiguous ``(S, V)`` tensor (``S = scale * max_batch_size``)
+    and two shared counters:
+
+    * ``_read_pos`` — next row index the consumer will read from.
+    * ``_tail`` — upper bound (in unwrapped space) up to which the consumer
+      may read without stalling.
+
+    On init the buffer is fully filled and ``_tail = S``.  The consumer
+    advances ``_read_pos`` after each call; when it approaches the end of the
+    buffer it wraps back to 0.  Each consumed segment is enqueued for the
+    background thread to refill, which increments ``_tail`` once done.
+
+    Args:
+        vocab_size: Number of columns ``V``.
+        max_batch_size: Maximum rows per :meth:`get_rows` call ``B``.
+        scale: Buffer depth multiplier; ``S = scale * B``.  Must be >= 2 so
+            there is always at least one full batch of pre-filled rows ahead
+            of the consumer.
+    """
+
+    def __init__(
+        self,
+        vocab_size: int,
+        max_batch_size: int,
+        scale: int = 4,
+    ) -> None:
+        assert scale >= 2, "scale must be >= 2"
+        self._V = vocab_size
+        self._B = max_batch_size
+        self._S = scale * max_batch_size
+
+        # buffer allocation
+        self._buf = torch.empty(self._S, self._V, dtype=torch.float32)
+
+        # first-time buffer initialization
+        self._refill_slice(0, self._S)
+        self._tail: int = self._S
+        self._read_pos: int = 0
+
+        # _tail and _read_pos are guarded by _cond.
+        self._cond = threading.Condition(threading.Lock())
+
+        # Refill requests: (start, end, wrap)
+        self._refill_q: queue.Queue[tuple[int, int, bool] | None] = queue.Queue()
+
+        self._thread = threading.Thread(target=self._produce, daemon=True)
+        self._thread.start()
+
+    @abstractmethod
+    def _refill_slice(self, start: int, end: int) -> None:
+        """Fill ``self._buf[start:end]`` with fresh values in-place."""
+        ...
+
+    @property
+    def vocab_size(self) -> int:
+        return self._V
+
+    @contextlib.contextmanager
+    def borrow_rows(self, n: int) -> Generator[torch.Tensor, None, None]:
+        """Context manager that yields a zero-copy ``(n, V)`` view.
+
+        The backing rows are released for refill automatically when the
+        ``with`` block exits, even if an exception is raised.  The view
+        must not be used after the block.
+
+        Args:
+            n: Number of rows to borrow.  Must satisfy ``1 <= n <= B``.
+
+        Example::
+
+            with buf.borrow_rows(batch_size) as noise:
+                tokens = probs.div(noise).argmax(dim=-1)
+        """
+        start = self._read_pos
+        end = start + n
+
+        # wait for the consumer to fill up at least n many values ahead
+        with self._cond:
+            self._cond.wait_for(lambda: self._tail >= end)
+
+        # get view (zero-copy)
+        view = self._buf[start:end]
+
+        wrap: bool = end > self._S - self._B
+        if wrap:
+            with self._cond:
+                self._tail -= self._S
+
+            self._read_pos = 0
+        else:
+            self._read_pos = end
+
+        try:
+            # yield view to outside consumer
+            yield view
+        finally:
+            # issue refill request once view has been consumed and returned
+            self._refill_q.put((start, end, wrap))
+
+    def _produce(self) -> None:
+        while True:
+            req = self._refill_q.get()
+
+            # handle termination signal
+            if req is None:
+                break
+
+            # refill buffer
+            start, end, wrap = req
+            self._refill_slice(start, end)
+
+            increment = (self._S - start) if wrap else (end - start)
+            with self._cond:
+                self._tail += increment
+                self._cond.notify_all()
+
+    def stop(self) -> None:
+        """Signal the background thread to stop and wait for it to exit."""
+        self._refill_q.put(None)
+        self._thread.join()
+
+
+class AsyncExponential_RingBuffer(AsyncRingBuffer):
+    """Ring buffer that pre-generates exponential noise via ``exponential_()``."""
+
+    def _refill_slice(self, start: int, end: int) -> None:
+        self._buf[start:end].exponential_()
+
+
+class _AsyncCounterRingBuffer(AsyncRingBuffer):
+    """Ring buffer that fills each row with the cumulative row index.
+
+    Used in tests to verify that consumers receive the correct rows in order
+    without repeating any.
+    """
+
+    def __init__(self, vocab_size: int, max_batch_size: int, scale: int = 4) -> None:
+        self._total_generated: int = 0
+        super().__init__(vocab_size, max_batch_size, scale)
+
+    def _refill_slice(self, start: int, end: int) -> None:
+        n = end - start
+        for i in range(n):
+            self._buf[start + i].fill_(self._total_generated)
+            self._total_generated += 1

tests/v1/sample/test_async_ring_buffer.py
@@ -1,0 +1,134 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the sendnn-inference project
+
+import pytest
+from spyre_inference.v1.sample.async_ring_buffer import (
+    AsyncExponential_RingBuffer,
+    AsyncRingBuffer,
+    _AsyncCounterRingBuffer,
+)
+
+
+class TestAsyncRingBuffer:
+    """Tests for AsyncRingBuffer, AsyncExponentialRingBuffer, and
+    _AsyncCounterRingBuffer."""
+
+    def test_abc_cannot_instantiate(self):
+        """AsyncRingBuffer is abstract and must not be instantiated directly."""
+        with pytest.raises(TypeError):
+            AsyncRingBuffer(vocab_size=10, max_batch_size=4)  # type: ignore[abstract]
+
+    def test_counter_no_duplicates(self):
+        """Every row index must be yielded at most once across all borrows.
+
+        The counter buffer does NOT guarantee strict global sequentiality:
+        rows near the wrap boundary can be skipped.  The invariant it *does*
+        guarantee is that no row index is ever handed to the consumer twice.
+        """
+        V, B, scale = 3, 4, 4
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        total_steps = 20
+        seen: list[int] = []
+        try:
+            for _ in range(total_steps):
+                with buf.borrow_rows(1) as rows:
+                    assert rows.shape == (1, V)
+                    # All columns of a row share the same counter value.
+                    val = int(rows[0, 0].item())
+                    seen.append(val)
+        finally:
+            buf.stop()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_counter_variable_batch_sizes(self):
+        """No row index must appear twice across borrows of varying size."""
+        V, B, scale = 2, 4, 4
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        batch_sizes = [1, 2, 3, 4, 1, 4, 2, 3]
+        seen: list[int] = []
+        try:
+            for b in batch_sizes:
+                with buf.borrow_rows(b) as rows:
+                    assert rows.shape == (b, V)
+                    for i in range(b):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.stop()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_counter_wrap_around(self):
+        """No row index is repeated when the buffer wraps multiple times."""
+        V, B, scale = 2, 2, 4  # S = 8
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        n_steps = 5 * scale
+        seen: list[int] = []
+        try:
+            for _ in range(n_steps):
+                with buf.borrow_rows(B) as rows:
+                    for i in range(B):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.stop()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_exponential_shape_and_positivity(self):
+        """AsyncExponentialRingBuffer returns correctly shaped positive tensors."""
+        V, B = 16, 4
+        buf = AsyncExponential_RingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            for b in [1, 2, B]:
+                with buf.borrow_rows(b) as rows:
+                    assert rows.shape == (b, V)
+                    assert (rows > 0).all(), "exponential values must be positive"
+        finally:
+            buf.stop()
+
+    def test_borrow_is_zero_copy(self):
+        """borrow_rows must yield a view into the backing buffer, not a copy."""
+        V, B = 8, 4
+        buf = AsyncExponential_RingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            with buf.borrow_rows(B) as rows:
+                assert rows.untyped_storage().data_ptr() == buf._buf.untyped_storage().data_ptr()
+        finally:
+            buf.stop()
+
+    def test_release_on_exception(self):
+        """Release must occur even when the consumer body raises."""
+        V, B = 4, 2
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            with pytest.raises(RuntimeError, match="intentional"):
+                with buf.borrow_rows(B):
+                    raise RuntimeError("intentional")
+            # If release happened, a second borrow must succeed.
+            with buf.borrow_rows(B) as rows:
+                assert rows.shape == (B, V)
+        finally:
+            buf.stop()
+
+    def test_stop_joins_thread(self):
+        """stop() must cause the background thread to finish."""
+        buf = AsyncExponential_RingBuffer(vocab_size=4, max_batch_size=2)
+        assert buf._thread.is_alive()
+        buf.stop()
+        assert not buf._thread.is_alive()
+
+    @pytest.mark.parametrize("scale", [2, 3, 4, 8])
+    def test_scale_invariance(self, scale: int):
+        """Different scale values must all produce no duplicate row indices."""
+        V, B = 4, 3
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        seen: list[int] = []
+        try:
+            for _ in range(scale * 3):
+                with buf.borrow_rows(B) as rows:
+                    for i in range(B):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.stop()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"

vllm-project/vllm-spyre

Signed-off-by: David Holtz <56723830+dmholtz@users.noreply.github.com>

sendnn_inference/v1/sample/async_ring_buffer.py
@@ -1,0 +1,160 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the sendnn-inference project
+
+import contextlib
+import queue
+import threading
+from abc import ABC, abstractmethod
+from collections.abc import Generator
+
+import torch
+
+
+class AsyncRingBuffer(ABC):
+    """Pre-generates data rows on a background thread via a ring buffer.
+
+    Maintains a contiguous ``(S, V)`` tensor (``S = scale * max_batch_size``)
+    and two shared counters:
+
+    * ``_read_pos`` — next row index the consumer will read from.
+    * ``_tail`` — upper bound (in unwrapped space) up to which the consumer
+      may read without stalling.
+
+    On init the buffer is fully filled and ``_tail = S``.  The consumer
+    advances ``_read_pos`` after each call; when it approaches the end of the
+    buffer it wraps back to 0.  Each consumed segment is enqueued for the
+    background thread to refill, which increments ``_tail`` once done.
+
+    Args:
+        vocab_size: Number of columns ``V``.
+        max_batch_size: Maximum rows per :meth:`get_rows` call ``B``.
+        scale: Buffer depth multiplier; ``S = scale * B``.  Must be >= 2 so
+            there is always at least one full batch of pre-filled rows ahead
+            of the consumer.
+    """
+
+    def __init__(
+        self,
+        vocab_size: int,
+        max_batch_size: int,
+        scale: int = 4,
+    ) -> None:
+        assert scale >= 2, "scale must be >= 2"
+        self._V = vocab_size
+        self._B = max_batch_size
+        self._S = scale * max_batch_size
+
+        # buffer allocation
+        self._buf = torch.empty(self._S, self._V, dtype=torch.float32)
+
+        # first-time buffer initialization
+        self._refill_slice(0, self._S)
+        self._tail: int = self._S
+        self._read_pos: int = 0
+
+        # _tail and _read_pos are guarded by _cond.
+        self._cond = threading.Condition(threading.Lock())
+
+        # Refill requests: (start, end, wrap)
+        self._refill_q: queue.Queue[tuple[int, int, bool] | None] = queue.Queue()
+
+        self._thread = threading.Thread(target=self._produce, daemon=True)
+        self._thread.start()
+
+    @abstractmethod
+    def _refill_slice(self, start: int, end: int) -> None:
+        """Fill ``self._buf[start:end]`` with fresh values in-place."""
+        ...
+
+    @property
+    def vocab_size(self) -> int:
+        return self._V
+
+    @contextlib.contextmanager
+    def borrow_rows(self, n: int) -> Generator[torch.Tensor, None, None]:
+        """Context manager that yields a zero-copy ``(n, V)`` view.
+
+        The backing rows are released for refill automatically when the
+        ``with`` block exits, even if an exception is raised.  The view
+        must not be used after the block.
+
+        Args:
+            n: Number of rows to borrow.  Must satisfy ``1 <= n <= B``.
+
+        Example::
+
+            with buf.borrow_rows(batch_size) as noise:
+                tokens = probs.div(noise).argmax(dim=-1)
+        """
+        start = self._read_pos
+        end = start + n
+
+        # wait for the consumer to fill up at least n many values ahead
+        with self._cond:
+            self._cond.wait_for(lambda: self._tail >= end)
+
+        # get view (zero-copy)
+        view = self._buf[start:end]
+
+        wrap: bool = end > self._S - self._B
+        if wrap:
+            with self._cond:
+                self._tail -= self._S
+
+            self._read_pos = 0
+        else:
+            self._read_pos = end
+
+        try:
+            # yield view to outside consumer
+            yield view
+        finally:
+            # issue refill request once view has been consumed and returned
+            self._refill_q.put((start, end, wrap))
+
+    def _produce(self) -> None:
+        while True:
+            req = self._refill_q.get()
+
+            # handle termination signal
+            if req is None:
+                break
+
+            # refill buffer
+            start, end, wrap = req
+            self._refill_slice(start, end)
+
+            increment = (self._S - start) if wrap else (end - start)
+            with self._cond:
+                self._tail += increment
+                self._cond.notify_all()
+
+    def shutdown(self) -> None:
+        """Signal the background thread to stop and wait for it to exit."""
+        self._refill_q.put(None)
+        self._thread.join()
+
+
+class AsyncExponential_RingBuffer(AsyncRingBuffer):
+    """Ring buffer that pre-generates exponential noise via ``exponential_()``."""
+
+    def _refill_slice(self, start: int, end: int) -> None:
+        self._buf[start:end].exponential_()
+
+
+class _AsyncCounterRingBuffer(AsyncRingBuffer):
+    """Ring buffer that fills each row with the cumulative row index.
+
+    Used in tests to verify that consumers receive the correct rows in order
+    without repeating any.
+    """
+
+    def __init__(self, vocab_size: int, max_batch_size: int, scale: int = 4) -> None:
+        self._total_generated: int = 0
+        super().__init__(vocab_size, max_batch_size, scale)
+
+    def _refill_slice(self, start: int, end: int) -> None:
+        n = end - start
+        for i in range(n):
+            self._buf[start + i].fill_(self._total_generated)
+            self._total_generated += 1

tests/v1/sample/test_async_ring_buffer.py
@@ -1,0 +1,135 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the sendnn-inference project
+
+import pytest
+
+from sendnn_inference.v1.sample.async_ring_buffer import (
+    AsyncExponential_RingBuffer,
+    AsyncRingBuffer,
+    _AsyncCounterRingBuffer,
+)
+
+
+class TestAsyncRingBuffer:
+    """Tests for AsyncRingBuffer, AsyncExponentialRingBuffer, and
+    _AsyncCounterRingBuffer."""
+
+    def test_abc_cannot_instantiate(self):
+        """AsyncRingBuffer is abstract and must not be instantiated directly."""
+        with pytest.raises(TypeError):
+            AsyncRingBuffer(vocab_size=10, max_batch_size=4)  # type: ignore[abstract]
+
+    def test_counter_no_duplicates(self):
+        """Every row index must be yielded at most once across all borrows.
+
+        The counter buffer does NOT guarantee strict global sequentiality:
+        rows near the wrap boundary can be skipped.  The invariant it *does*
+        guarantee is that no row index is ever handed to the consumer twice.
+        """
+        V, B, scale = 3, 4, 4
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        total_steps = 20
+        seen: list[int] = []
+        try:
+            for _ in range(total_steps):
+                with buf.borrow_rows(1) as rows:
+                    assert rows.shape == (1, V)
+                    # All columns of a row share the same counter value.
+                    val = int(rows[0, 0].item())
+                    seen.append(val)
+        finally:
+            buf.shutdown()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_counter_variable_batch_sizes(self):
+        """No row index must appear twice across borrows of varying size."""
+        V, B, scale = 2, 4, 4
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        batch_sizes = [1, 2, 3, 4, 1, 4, 2, 3]
+        seen: list[int] = []
+        try:
+            for b in batch_sizes:
+                with buf.borrow_rows(b) as rows:
+                    assert rows.shape == (b, V)
+                    for i in range(b):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.shutdown()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_counter_wrap_around(self):
+        """No row index is repeated when the buffer wraps multiple times."""
+        V, B, scale = 2, 2, 4  # S = 8
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        n_steps = 5 * scale
+        seen: list[int] = []
+        try:
+            for _ in range(n_steps):
+                with buf.borrow_rows(B) as rows:
+                    for i in range(B):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.shutdown()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"
+
+    def test_exponential_shape_and_positivity(self):
+        """AsyncExponentialRingBuffer returns correctly shaped positive tensors."""
+        V, B = 16, 4
+        buf = AsyncExponential_RingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            for b in [1, 2, B]:
+                with buf.borrow_rows(b) as rows:
+                    assert rows.shape == (b, V)
+                    assert (rows > 0).all(), "exponential values must be positive"
+        finally:
+            buf.shutdown()
+
+    def test_borrow_is_zero_copy(self):
+        """borrow_rows must yield a view into the backing buffer, not a copy."""
+        V, B = 8, 4
+        buf = AsyncExponential_RingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            with buf.borrow_rows(B) as rows:
+                assert rows.untyped_storage().data_ptr() == buf._buf.untyped_storage().data_ptr()
+        finally:
+            buf.shutdown()
+
+    def test_release_on_exception(self):
+        """Release must occur even when the consumer body raises."""
+        V, B = 4, 2
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B)
+        try:
+            with pytest.raises(RuntimeError, match="intentional") as _:
+                with buf.borrow_rows(B):
+                    raise RuntimeError("intentional")
+        finally:
+            # If release happened, a second borrow must succeed.
+            with buf.borrow_rows(B) as rows:
+                assert rows.shape == (B, V)
+            buf.shutdown()
+
+    def test_stop_joins_thread(self):
+        """stop() must cause the background thread to finish."""
+        buf = AsyncExponential_RingBuffer(vocab_size=4, max_batch_size=2)
+        assert buf._thread.is_alive()
+        buf.shutdown()
+        assert not buf._thread.is_alive()
+
+    @pytest.mark.parametrize("scale", [2, 3, 4, 8])
+    def test_scale_invariance(self, scale: int):
+        """Different scale values must all produce no duplicate row indices."""
+        V, B = 4, 3
+        buf = _AsyncCounterRingBuffer(vocab_size=V, max_batch_size=B, scale=scale)
+        seen: list[int] = []
+        try:
+            for _ in range(scale * 3):
+                with buf.borrow_rows(B) as rows:
+                    for i in range(B):
+                        seen.append(int(rows[i, 0].item()))
+        finally:
+            buf.shutdown()
+
+        assert len(seen) == len(set(seen)), f"duplicate row indices returned: {seen}"